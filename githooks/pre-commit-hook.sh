bb6b74b4e3b51bb1752337b2610c168ec9ecc497
Joe Cale (Ana Laptop) <31443+joebone@users.noreply.github.com>
2021-09-20 19:29:33 +0200

    Husky tests

--- githooks/pre-commit-hook.sh
+++ githooks/pre-commit-hook.sh
@@ -1,25 +1,45 @@
 #!/bin/sh
 
 
+# printf "Hello from the hook!"
 
 
-changedFiles="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+changedFiles="$(git diff-tree -r --name-status --no-commit-id ORIG_HEAD HEAD)"
+
+printf "$changedFiles\n"
 
 runOnChange() {
 	echo "$changedFiles" | grep -q "$1" && echo -e "$2"
 }
 
+haschanged() {
+    # we are only interested in modified or added status, not deleted.
+  exists=$(echo "$changedFiles" | grep -E "[AM].*$1.*");
+  if [ -z $exists ]
+  then
+    # 1 = false
+    return 1
+  else
+    # 0 = true    
+    
+    return 0
+  fi
+}
 
-read -r -d '' packageLockWarning <<- EOM
-    \033[35m*******************************************************************************
-    * \033[33mpackage-lock.json\033[35m has changed. Run\033[33m yarn --ignore-optional\033[35m to update your dependencies. *
-    *******************************************************************************
-EOM
 
+if haschanged "package-lock.json"; then
+    printf "USE YARN TO INSTALL PACKAGES, NOT NPM: \033[33m yarn --ignore-optional \033[35m !. \033[31m Delete \033[35m your \033[31m package-lock.json \033[35m file.\n"
+    exit 1;
+fi
 
-runOnChange package-lock.json "$packageLockWarning"
+#warn "caca"
+#TXT=$(warn "package-lock.json") && runOnChange package-lock.json "$TEXT"
+#TXT=$(warn "yarn-lock.json") && runOnChange yarn-lock.json "$TEXT"
 
-echo "NO COMMITS!"
-exit 1;
+# run tests, or whatever..
 
+# yarn --ignore-optional
+# yarn build
+
+# echo "All good, commit is fine."
 # exit 1
