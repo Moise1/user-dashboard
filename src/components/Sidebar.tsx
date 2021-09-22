cc4dfdb4a988ba616436db791c15a92ca3157600
sharukhk08 <iamsharukh08@gmail.com>
2021-09-22 13:41:37 +0530

    delete comments-codes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -25,6 +25,7 @@ import {
   SettingsIcon,
   CircleDotIcon,
   HelpIcon,
+  LogOutIcon,
 } from "../components/common/Icons";
 import {
   UserOutlined,
@@ -47,7 +48,6 @@ export default function Sidebar(props: Props) {
   const history = useHistory();
 
   const routeChange = () => {
-    let path = `newPath`;
     history.push("/home");
   };
   const { collapsed, toggle, staticvalue, togglestatic } = props;
@@ -108,45 +108,21 @@ export default function Sidebar(props: Props) {
             <Menu.Item
               key="1"
               style={{ color: "#000", fontSize: "18px", fontWeight: "bold" }}
-              icon={
-                // <img
-                //   className="active-white-svg"
-                //   src={dashboard}
-                //   height={20}
-                //   alt=""
-                // />
-                <DashBoardIcon />
-              }
+              icon={<DashBoardIcon />}
             >
               {t("ds")}
             </Menu.Item>
             <Menu.Item
               style={{ color: "#000", fontSize: "18px", fontWeight: "bold" }}
               key="2"
-              icon={
-                // <img
-                //   className="active-white-svg"
-                //   src={catalog}
-                //   height={20}
-                //   alt=""
-                // />
-                <CatalogIcon />
-              }
+              icon={<CatalogIcon />}
             >
               <span className="sidebar_element">{t("cat")}</span>
             </Menu.Item>
             <Menu.Item
               style={{ color: "#000", fontSize: "18px", fontWeight: "bold" }}
               key="3"
-              icon={
-                // <img
-                //   className="active-white-svg"
-                //   src={listnow}
-                //   height={20}
-                //   alt=""
-                // />
-                <ListNowIcon />
-              }
+              icon={<ListNowIcon />}
             >
               {t("ln")}
             </Menu.Item>
@@ -154,10 +130,6 @@ export default function Sidebar(props: Props) {
               key="4"
               style={{ color: "#000", fontSize: "18px", fontWeight: "bold" }}
               icon={
-                // <i
-                //   onClick={routeChange}
-                //   className="fas active-white-svg fa-list d-blue"
-                // ></i>
                 <span onClick={routeChange}>
                   <ListingsIcon />
                 </span>
@@ -177,199 +149,114 @@ export default function Sidebar(props: Props) {
               icon={<SettingsIcon />}
               title="Settings"
             >
-              {/* {t("set")} <i className="fas fa-sort-down text-dark ml-2"></i> */}
               <Menu.Item key="9">
                 <li className="list-unstyled list-items-hover fw-400 m-0 h-25 leading-25">
-                  {" "}
                   <span className="mr-3">
                     <CircleDotIcon />
-                  </span>{" "}
+                  </span>
                   Channel
                 </li>
               </Menu.Item>
               <Menu.Item key="10">
                 <li className="list-unstyled list-items-hover fw-400 m-0 h-25 leading-25">
-                  {" "}
                   <span className="mr-3">
                     <CircleDotIcon />
-                  </span>{" "}
+                  </span>
                   Sources
                 </li>
               </Menu.Item>
               <Menu.Item key="11">
                 <li className="list-unstyled list-items-hover fw-400 m-0 h-25 leading-25">
-                  {" "}
                   <span className="mr-3">
                     <CircleDotIcon />
-                  </span>{" "}
+                  </span>
                   Pricing Rules
                 </li>
               </Menu.Item>
               <Menu.Item key="13">
                 <li className="list-unstyled list-items-hover fw-400 m-0 h-25 leading-25">
-                  {" "}
                   <span className="mr-3">
                     <CircleDotIcon />
-                  </span>{" "}
+                  </span>
                   Browser Extensions
                 </li>
               </Menu.Item>
               <Menu.Item key="14">
                 <li className="list-unstyled list-items-hover fw-400 m-0 h-25 leading-25">
-                  {" "}
                   <span className="mr-3">
                     <CircleDotIcon />
-                  </span>{" "}
+                  </span>
                   VA Profiles
                 </li>
               </Menu.Item>
               <Menu.Item key="15">
                 <li className="list-unstyled list-items-hover fw-400 m-0 h-25 leading-25">
-                  {" "}
                   <span className="mr-3">
                     <CircleDotIcon />
-                  </span>{" "}
+                  </span>
                   Templates
                 </li>
               </Menu.Item>
             </SubMenu>
-            {/* <Menu.Item key="5" icon={<img src={settings} height={20} alt="" />}>
-          {t("set")} <i className="fas fa-sort-down text-dark ml-2"></i>
-        </Menu.Item> */}
             {/* SERVICES  */}
             <SubMenu
               key="sub4"
               style={{ color: "#000", fontSize: "18px", fontWeight: "bold" }}
-              icon={
-                // <img
-                //   className="active-white-svg"
-                //   src={services}
-                //   height={20}
-                //   alt=""
-                // />
-                <ServiceIcon />
-              }
+              icon={<ServiceIcon />}
               title="Services"
             >
-              {/* {t("set")} <i className="fas fa-sort-down text-dark ml-2"></i> */}
-
               <Menu.Item key="13">
                 <li className="list-unstyled list-items-hover fw-400 m-0 h-25 leading-25">
-                  {" "}
                   <span className="mr-3">
                     <CircleDotIcon />
-                  </span>{" "}
+                  </span>
                   VA Profiles
                 </li>
               </Menu.Item>
               <Menu.Item key="14">
                 <li className="list-unstyled list-items-hover fw-400 m-0 h-25 leading-25">
-                  {" "}
                   <span className="mr-3">
                     <CircleDotIcon />
-                  </span>{" "}
+                  </span>
                   VA Profiles
                 </li>
               </Menu.Item>
               <Menu.Item key="15">
                 <li className="list-unstyled list-items-hover fw-400 m-0 h-25 leading-25">
-                  {" "}
                   <span className="mr-3">
                     <CircleDotIcon />
-                  </span>{" "}
+                  </span>
                   VA Profiles
                 </li>
               </Menu.Item>
             </SubMenu>
-            {/* <Menu.Item
-          style={{ color: "#000", fontSize: "18px", fontWeight: "bold" }}
-          key="6"
-          icon={
-            <img
-              className="active-white-svg"
-              src={services}
-              height={20}
-              alt=""
-            />
-          }
-        >
-          {t("srvc")} <i className="fas fa-sort-down text-dark ml-2"></i>
-        </Menu.Item> */}
             <SubMenu
               key="sub5"
               style={{ color: "#000", fontSize: "18px", fontWeight: "bold" }}
               icon={<HelpIcon />}
               title="Help"
             >
-              {/* {t("set")} <i className="fas fa-sort-down text-dark ml-2"></i> */}
               <Menu.Item key="9">
                 <li className="list-unstyled list-items-hover fw-400 m-0 h-25 leading-25">
-                  {" "}
                   <span className="mr-3">
                     <CircleDotIcon />
-                  </span>{" "}
+                  </span>
                   Templates
                 </li>
               </Menu.Item>
               <Menu.Item key="10">
                 <li className="list-unstyled list-items-hover fw-400 m-0 h-25 leading-25">
-                  {" "}
                   <span className="mr-3">
                     <CircleDotIcon />
-                  </span>{" "}
+                  </span>
                   VA Profiles
                 </li>
               </Menu.Item>
             </SubMenu>
-            {/* <Menu.Item
-          style={{ color: "#000", fontSize: "18px", fontWeight: "bold" }}
-          key="7"
-          icon={
-            <img className="active-white-svg" src={help} height={20} alt="" />
-          }
-        >
-          {t("hlp")} <i className="fas fa-sort-down text-dark ml-4 pl-1"></i>
-        </Menu.Item> */}
           </Menu>
           <div className="logout-icon d-flex justify-content-end align-items-center z-10">
             <span className="mx-2 ">
-              <svg
-                id="Group_84"
-                data-name="Group 84"
-                xmlns="http://www.w3.org/2000/svg"
-                width="26"
-                height="26"
-                viewBox="0 0 31.622 31.622"
-              >
-                <path
-                  id="Path_17"
-                  data-name="Path 17"
-                  d="M0,0H31.622V31.622H0Z"
-                  fill="none"
-                />
-                <path
-                  id="Path_18"
-                  data-name="Path 18"
-                  d="M17.494,9.27V6.635A2.635,2.635,0,0,0,14.858,4H5.635A2.635,2.635,0,0,0,3,6.635V22.446a2.635,2.635,0,0,0,2.635,2.635h9.223a2.635,2.635,0,0,0,2.635-2.635V19.811"
-                  transform="translate(0.953 1.27)"
-                  fill="none"
-                  stroke="#ef6092"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                />
-                <path
-                  id="Path_19"
-                  data-name="Path 19"
-                  d="M7,12.953H25.446L21.494,9m0,7.906,3.953-3.953"
-                  transform="translate(2.223 2.858)"
-                  fill="none"
-                  stroke="#ef6092"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                />
-              </svg>
+              <LogOutIcon />
             </span>
 
             <span className="d-none-icon">Logout</span>
