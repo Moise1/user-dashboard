be8da872795350484120f16d46aca8d5b56ab312
sharukhk08 <iamsharukh08@gmail.com>
2021-10-07 09:23:40 +0530

    ui

--- src/components/common/SearchWithButton.tsx
+++ src/components/common/SearchWithButton.tsx
@@ -6,9 +6,9 @@ import SearchInput from '../SmallComponents/SearchInput';
 const SearchWithButton = () => {
   return (
     <>
-      <div className="d-flex">
+      <div className="d-flex flex-column flex-sm-row">
         <SearchInput />
-        <div className="ml-3">
+        <div className="ml-sm-3 ml-0">
           <button className="btn advance-search-purple-button">
             <img src={list_saerch} className="invert mr-3 " height="26" alt="" />
             {t('AdvancedSearch')}
--- src/components/sources/Sources.tsx
+++ src/components/sources/Sources.tsx
@@ -1,14 +1,49 @@
 import React from 'react';
-import { LeftBackArrowIcon } from '../common/Icons';
+// import { Dropdown } from 'react-bootstrap';
+import { DispatchedOrderIcon, LeftBackArrowIcon } from '../common/Icons';
 import SearchWithButton from '../common/SearchWithButton';
+import Select from 'react-select';
+// import { Menu } from 'antd';
+// import search_icon from '../../assets/search.svg';
+// import HeaderDropDownItem from '../SmallComponents/HeaderDropDownItem';
+// import { Dropdown, Button, Space } from 'antd';
+// import DropDownMenu from '../SmallComponents/DropDownMenu';
 
 const Sources = () => {
+  // const [supplier, setSupplier] = useState({});
+  // const [checked, setChecked] = useState(false);
+  const options = [
+    { value: 'Select supplier', label: 'Select supplier', isDisabled: true },
+    { value: 'Amazon', label: 'Amazon' },
+    { value: 'Flipcart', label: 'Flipcart' },
+    { value: 'Ali Express', label: 'Ali Express' }
+  ];
+  // const handleSupplier = (e: any) => {
+  //   console.log(e.target.value);
+  //   // setSupplier(e);
+  // };
+  // console.log(supplier);
+
+  // const handleChange = (: Value) => {
+  //   setChecked(e.target.checked);
+  // };
+
+  // const [state, setState] = useState({ value: false });
+
+  // const handleChange = (event: ChangeEvent<{ value: boolean }>) => {
+  //   setState({ value: event?.currentTarget?.value });
+  // };
+
+  // const offstyle = 'btn-danger';
+  // const onstyle = 'btn-success';
+  // const displayStyle = state ? onstyle : offstyle;
+
   return (
     <>
       <div className="w-100 p-3">
         <SearchWithButton />
 
-        <div className="d-flex mt-4">
+        <div className="d-flex flex-column flex-sm-row mt-4">
           <h2 className="back-to-supplier-heading d-flex align-items-center">
             <span>
               <LeftBackArrowIcon />
@@ -22,10 +57,37 @@ const Sources = () => {
           </div>
         </div>
 
-        <div className="auto">
+        <div className="auto-ordering-section my-3">
           <h2 className="auto-ordering-heading-text">
             Autoordering supplier configuration: <span>select a supplier from the list</span>
           </h2>
+          <div className="row">
+            <div className="col-12 col-sm-4 col-xl-3">
+              <Select options={options} defaultValue={options[0]} />
+            </div>
+          </div>
+
+          <div className="d-flex justify-content-between">
+            <div className="d-flex flex-column">
+              <div className="d-flex">
+                <div className="enable-disable-para ">
+                  <p>Enable/Disable Auto-ordering</p>
+                  <span>Disabling auto-ordering will require you to manually process new orders.</span>
+                </div>
+
+                <span>Toggle button</span>
+              </div>
+
+              <div>Account configuration: Select or add account</div>
+            </div>
+            <div className="d-flex flex-column">
+              <button className="btn save-changes-btn mb-3">
+                <DispatchedOrderIcon />
+                <span className="ml-2">Save changes</span>
+              </button>
+              <button className="btn reset-to-default-btn">Reset to default</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
