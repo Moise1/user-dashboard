915bd56a09c6c4ebb10004838f902a9b9e209165
sharukhk08 <iamsharukh08@gmail.com>
2021-10-07 14:41:31 +0530

    sources reponsive -ness

--- src/components/sources/AccountsInput.tsx
+++ src/components/sources/AccountsInput.tsx
@@ -5,56 +5,76 @@ const AccountsInput = () => {
   return (
     <>
       <div className="row">
-        <div className="col-6">
-          <label className="account-label-style" htmlFor="">
-            Alias
-          </label>
-          <input className="account-input-style" type="text" placeholder="Dad Account" />
-        </div>
         <div className="col-12">
-          <div className="row">
-            <div className="col-3">
-              <label className="account-label-style" htmlFor="">
-                Amazon login
-              </label>
-              <input className="account-input-style" type="text" placeholder="Dad Account" />
-            </div>
-            <div className="col">
-              <label className="account-label-style" htmlFor="">
-                Amazon password
-              </label>
-              <input className="account-input-style" type="text" placeholder="Dad Account" />
-            </div>
+          <div className="width-207">
+            <label className="account-label-style" htmlFor="">
+              Alias
+            </label>
+            <input className="account-input-style" type="text" placeholder="Dad Account" />
           </div>
         </div>
         <div className="col-12">
-          <label className="account-label-style" htmlFor="">
-            OTP code(2FA) <span>Obtain your OTP code </span>
-          </label>
-          <input className="account-input-style" type="text" placeholder="Dad Account" />
+          <div className="d-flex flex-column flex-sm-row">
+            <div className="acc-input-parent">
+              <label className="account-label-style" htmlFor="">
+                Amazon login
+              </label>
+              <input className="amazonlogin-input-style" type="text" placeholder="dadaccount@gmail.com" />
+            </div>
+            <div className="width-207 ml-0 ml-sm-5">
+              <label className="account-label-style" htmlFor="">
+                Amazon password
+              </label>
+              <input className="account-input-style" type="text" placeholder="3456344" />
+            </div>
+          </div>
         </div>
+
         <div className="col-12">
-          <label className="account-label-style" htmlFor="">
-            Phone number
-          </label>
-          <input className="account-input-style" type="text" placeholder="Dad Account" />
+          <div className="width-466">
+            <label className="account-label-style" htmlFor="">
+              OTP code(2FA) <span className="ml-2 obtain-code-otp">Obtain your OTP code </span>
+            </label>
+            <input className="otp-code-input-styles" type="text" placeholder="JJSndfnfgurbgjD935h5gmSKFJASFNFNBGG" />
+          </div>
         </div>
+
         <div className="col-12">
+          <div className="width-207">
+            <label className="account-label-style" htmlFor="">
+              Phone number
+            </label>
+            <input className="account-input-style" type="text" placeholder="(555) 555-5555" />
+          </div>
+        </div>
+        <div className="col-12 my-5">
           <div className="d-flex">
             <div className="enable-disable-para ">
               <p>Enable/Disable Auto-ordering</p>
               <span>Disabling auto-ordering will require you to manually process new orders.</span>
             </div>
 
-            <span>Toggle button</span>
+            <div className="custom-control  d-flex align-items-center switchbox custom-switch ">
+              <label className="switch-toggle mb-0 " htmlFor="checkbox-1">
+                <input
+                  className="input-toggle-switch"
+                  // onChange={handleActive}
+                  // checked={status === undefined ? false : status}
+                  // checked={isActive}
+                  type="checkbox"
+                  id="checkbox-1"
+                />
+                <div className="slider-toggle round"></div>
+              </label>
+            </div>
           </div>
         </div>
         <div className="col-12">
-          <p>
+          <p className="remove-account-div">
             {' '}
-            <DustbinPinkIcon /> Remove account
+            <DustbinPinkIcon /> <div className="ml-2">Remove account</div>
           </p>
-          <p>
+          <p className="dark-pink-para mt-3">
             If you delete this account, you will have to set it up again and no payments will be made through this
             account.
           </p>
--- src/components/sources/Sources.tsx
+++ src/components/sources/Sources.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 // import { Dropdown } from 'react-bootstrap';
 import { DispatchedOrderIcon, LeftBackArrowIcon } from '../common/Icons';
 import SearchWithButton from '../common/SearchWithButton';
@@ -11,8 +11,9 @@ import AccountsInput from './AccountsInput';
 // import DropDownMenu from '../SmallComponents/DropDownMenu';
 
 const Sources = () => {
-  // const [supplier, setSupplier] = useState({});
-  // const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState<boolean>(false);
+  // const [supplier, setSupplier] = useState<string>('');
+
   const options = [
     { value: 'Select supplier', label: 'Select supplier', isDisabled: true },
     { value: 'Amazon', label: 'Amazon' },
@@ -23,25 +24,26 @@ const Sources = () => {
   //   console.log(e.target.value);
   //   // setSupplier(e);
   // };
+
+  // const handleSupplier = (event: ChangeEvent<HTMLInputElement>): void => {
+  //   setSupplier(event.target.value);
+  // };
+
   // console.log(supplier);
 
   // const handleChange = (: Value) => {
   //   setChecked(e.target.checked);
   // };
 
-  // const [state, setState] = useState({ value: false });
+  // FOR GET VALUE OF TOGGLE SWITCH
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setChecked(event.target.checked);
+  };
 
-  // const handleChange = (event: ChangeEvent<{ value: boolean }>) => {
-  //   setState({ value: event?.currentTarget?.value });
-  // };
-
-  // const offstyle = 'btn-danger';
-  // const onstyle = 'btn-success';
-  // const displayStyle = state ? onstyle : offstyle;
-
+  console.log(checked, 'state');
   return (
     <>
-      <div className="w-100 p-3">
+      <div className="w-100 p-3 ant-layout">
         <SearchWithButton />
 
         <div className="d-flex flex-column flex-sm-row mt-4">
@@ -68,22 +70,27 @@ const Sources = () => {
             </div>
           </div>
 
-          <div className="d-flex justify-content-between">
+          <div className="d-flex flex-column flex-lg-row justify-content-between mt-4 mt-lg-0">
             <div className="d-flex flex-column">
-              <div className="d-flex">
+              <div className="d-flex my-0 my-lg-5">
                 <div className="enable-disable-para ">
                   <p>Enable/Disable Auto-ordering</p>
                   <span>Disabling auto-ordering will require you to manually process new orders.</span>
                 </div>
 
-                <span>Toggle button</span>
+                <div className="custom-control  d-flex align-items-center switchbox custom-switch px-2">
+                  <label className="switch-toggle mb-0 " htmlFor="checkbox-2">
+                    <input className="input-toggle-switch" onChange={handleChange} type="checkbox" id="checkbox-2" />
+                    <div className="slider-toggle round"></div>
+                  </label>
+                </div>
               </div>
 
               <h2 className="acc-config-text">
-                Account configuration: <span>Select or add account</span>{' '}
+                Account configuration: <span> Dad account</span>{' '}
               </h2>
 
-              <div className="row">
+              <div className="row my-2">
                 <div className="col-7">
                   <Select options={options} defaultValue={options[0]} />
                 </div>
