62d1a7d948380c295eb1d0ee53bc25ebc8e8a635
sharukhk08 <iamsharukh08@gmail.com>
2021-10-12 13:44:38 +0530

    drop down

--- src/components/orders/OrderTable.tsx
+++ src/components/orders/OrderTable.tsx
@@ -140,9 +140,10 @@ function OrderTable(myProps: props) {
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu>
-                      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                      <Dropdown.Item>Process order</Dropdown.Item>
+                      <Dropdown.Item>Stop order</Dropdown.Item>
+                      <Dropdown.Item>Mark as dispatched</Dropdown.Item>
+                      <Dropdown.Item>Delete order</Dropdown.Item>
                     </Dropdown.Menu>
                   </Dropdown>
                 </td>
