292666e34aa3882cc1c9473dc70db8beb1e40fa2
sharukhk08 <iamsharukh08@gmail.com>
2021-10-14 13:56:54 +0530

    add new account to list

--- src/components/sources/AddAccountsSelect.tsx
+++ src/components/sources/AddAccountsSelect.tsx
@@ -18,9 +18,11 @@ const AddAccountsSelect = () => {
   ];
 
   const [showDropDown, setShowDropDown] = useState<boolean>();
+  const [showAccountInput, setShowAccountInput] = useState<boolean>();
   const [whatSelect, setWhatSelect] = useState<string>('Select Supplier');
   const [supplierData, setSupplierData] = useState(arrayLists);
   const [inputSearchValue, setInputSearchValue] = useState<string>('');
+  const [AddAccountValue, setAddAccountValue] = useState<string>('');
 
   const handleSelectValue = (value: string) => {
     setWhatSelect(value);
@@ -32,6 +34,20 @@ const AddAccountsSelect = () => {
     const filteredSearch = arrayLists.filter((obj) => obj.value.toLowerCase().includes(value.toLowerCase()));
     setSupplierData(filteredSearch);
   };
+
+  const handleAccountPushToList = () => {
+    setShowAccountInput(false);
+    const id = Math.random() * 1023812031111;
+
+    const newAccount = { value: AddAccountValue, id: id, status: 'active' };
+    console.log(newAccount, 'newAccount');
+    supplierData.unshift(newAccount);
+    // supplierData.splice(0, 0, newAccount);
+    console.log(arrayLists, 'arrayLists');
+  };
+
+  console.log(AddAccountValue);
+
   return (
     <>
       {showDropDown ? <div onClick={() => setShowDropDown(false)} className="overlay-select"></div> : ''}
@@ -59,12 +75,21 @@ const AddAccountsSelect = () => {
               </span>
             </div>
 
-            <div className="d-flex aling-items-center  mt-2">
-              <span className="add-acccounnt-span"> Add account</span>
-              <span className="ml-2">
-                <PersonWithPlusIcon />
-              </span>
-            </div>
+            {showAccountInput ? (
+              <div className="add-account-input-inselect">
+                <input type="text" className="position-relative" onChange={(e) => setAddAccountValue(e.target.value)} />
+                <span className="plus-icon-add" onClick={() => handleAccountPushToList()}>
+                  <PersonWithPlusIcon />
+                </span>
+              </div>
+            ) : (
+              <div className="d-flex aling-items-center  mt-2" onClick={() => setShowAccountInput(true)}>
+                <span className="add-acccounnt-span"> Add account</span>
+                <span className="ml-2">
+                  <PersonWithPlusIcon />
+                </span>
+              </div>
+            )}
 
             <div className="react-list-data-here">
               {supplierData.map((list) => (
