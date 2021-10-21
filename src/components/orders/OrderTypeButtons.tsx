e4559ae0ab68f34f9f57b50a215d20cb19c949b0
sharukhk08 <iamsharukh08@gmail.com>
2021-10-21 15:16:47 +0530

    delete unused css and remove hardcode content to t() Func

--- src/components/orders/OrderTypeButtons.tsx
+++ src/components/orders/OrderTypeButtons.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ProcessOrderIcon, HandStopOrderIcon, DustbinDeleteOrderIcon, DispatchedOrderIcon } from '../common/Icons';
+import { t } from '../../global/transShim';
 
 const OrderTypeButtons = () => {
   return (
@@ -8,21 +9,22 @@ const OrderTypeButtons = () => {
         <div className=" d-flex   justify-content-between">
           <button className="btn process-5-order-btn">
             <ProcessOrderIcon />
-            <span className="ml-2">Process 5 Orders</span>
+            <span className="ml-2"> {t('OrderButtons.Process5Orders')} </span>
           </button>
           <button className="btn stop-5-order-btn  mx-sm-3">
             <HandStopOrderIcon />
-            <span className="ml-2"> Stop 5 Orders</span>
+            <span className="ml-2"> {t('OrderButtons.Stop5Orders')} </span>
           </button>
         </div>
 
         <div className="d-flex  mt-xl-0 mt-4 justify-content-between ">
           <button className="btn delete-5-order-btn">
             <DustbinDeleteOrderIcon />
-            <span className="ml-2">Delete 5 Orders</span>
+            <span className="ml-2"> {t('OrderButtons.Delete5Orders')}</span>
           </button>
           <button className="btn mark-dispatch-btn  mx-sm-3">
-            <DispatchedOrderIcon />+<span className="ml-2">Mark as dispatched</span>
+            <DispatchedOrderIcon />
+            <span className="ml-2">{t('OrderButtons.MarkAsDispatched')}</span>
           </button>
         </div>
       </div>
