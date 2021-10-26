4786fe63ff69bd19705cc2976d78941301723427
sharukhk08 <iamsharukh08@gmail.com>
2021-10-26 11:38:59 +0530

    change layout of select supplier -source-setting & account configuration show when auto ordering is true

--- src/components/sources/AutoOrdering.tsx
+++ src/components/sources/AutoOrdering.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState } from 'react';
 import { DispatchedOrderIcon } from '../common/Icons';
 import AccountsInput from './AccountsInput';
 import AddAccountsSelect from './AddAccountsSelect';
@@ -6,11 +6,11 @@ import { t } from '../../global/transShim';
 
 interface props {
   whatSelect: string;
+  checked: boolean;
 }
 
 const AutoOrdering = (myProps: props) => {
-  const [checked, setChecked] = useState<boolean>(false);
-  const { whatSelect } = myProps;
+  const { whatSelect, checked } = myProps;
   const [DisableAccount, setDisableAccount] = useState<boolean>(false);
   const [whichAccountSelect, setwhichAccountSelect] = useState<string>('Select or add account');
   const [alias, setAlias] = useState<string>('Sheru');
@@ -18,45 +18,30 @@ const AutoOrdering = (myProps: props) => {
   const [otp, setOtp] = useState<string>('JJSndfnfgurbgjD935h5gmSKFJASFNF@#WEFSD');
   const [phone, setphone] = useState<string>('4442323423');
 
-  // FOR GET VALUE OF TOGGLE SWITCH
-  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    setChecked(event.target.checked);
-  };
-
-  console.log(checked, 'state');
-
   return (
     <>
       <div className="d-flex flex-column flex-lg-row justify-content-between mt-4 mt-lg-0">
         <div className="d-flex flex-column">
-          <div className="d-flex my-0 my-lg-5 ">
-            <div className="enable-disable-para ">
-              <p>{t('SourceConfigInputs.EnableDisableAutoOrdering')}</p>
-              <span className="mr-5">Disabling auto-ordering will require you to manually process new orders.</span>
-            </div>
+          {checked ? (
+            <>
+              <h2 className="acc-config-text py-3">
+                {t('SourceConfigInputs.AccountConfiguration')} : <span>{whichAccountSelect} </span>{' '}
+              </h2>
 
-            <div className="custom-control  d-flex align-items-center switchbox custom-switch px-2">
-              <label className="switch-toggle mb-0 " htmlFor="checkbox-2">
-                <input className="input-toggle-switch" onChange={handleChange} type="checkbox" id="checkbox-2" />
-                <div className="slider-toggle round"></div>
-              </label>
-            </div>
-          </div>
-
-          <h2 className="acc-config-text py-3">
-            {t('SourceConfigInputs.AccountConfiguration')} : <span>{whichAccountSelect} </span>{' '}
-          </h2>
-
-          <AddAccountsSelect
-            DisableAccount={DisableAccount}
-            setDisableAccount={setDisableAccount}
-            setwhichAccountSelect={setwhichAccountSelect}
-            whichAccountSelect={whichAccountSelect}
-            setAlias={setAlias}
-            setPassword={setPassword}
-            setOtp={setOtp}
-            setphone={setphone}
-          />
+              <AddAccountsSelect
+                DisableAccount={DisableAccount}
+                setDisableAccount={setDisableAccount}
+                setwhichAccountSelect={setwhichAccountSelect}
+                whichAccountSelect={whichAccountSelect}
+                setAlias={setAlias}
+                setPassword={setPassword}
+                setOtp={setOtp}
+                setphone={setphone}
+              />
+            </>
+          ) : (
+            ''
+          )}
 
           {/* IF ANY ACCOUNT IS NOT SELECTED AND VALUE IS NOT DEFAULT VALUE THEN WE HAVE NOT TO SHOW INPUT OR TOGGLE ICON  */}
           {whichAccountSelect.length && whichAccountSelect !== 'Select or add account' ? (
--- src/components/sources/Sources.tsx
+++ src/components/sources/Sources.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { LeftBackArrowIcon } from '../common/Icons';
 import SearchWithButton from '../common/SearchWithButton';
 import AutoOrdering from './AutoOrdering';
@@ -6,6 +6,7 @@ import SearchSelect from './SearchSelect';
 import { useHistory } from 'react-router-dom';
 import './Sources.css';
 import { SelectSupplierContext } from '../../contexts/SelectSupplierProvider';
+import { t } from '../../global/transShim';
 
 type ContextType = {
   supplierValue: string;
@@ -16,6 +17,7 @@ const Sources = () => {
   const [showOrdering, setShowOrdering] = useState<boolean>(false);
   const { supplierValue, setSupplierValue } = React.useContext(SelectSupplierContext) as ContextType;
   const [whatSelect, setWhatSelect] = useState<string>(supplierValue ? supplierValue : 'Select Supplier');
+  const [checked, setChecked] = useState<boolean>(false);
 
   const history = useHistory();
 
@@ -32,6 +34,11 @@ const Sources = () => {
     history.goBack();
   };
 
+  // FOR GET VALUE OF TOGGLE SWITCH
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <>
       <div className="w-100 p-sm-3 ant-layout mt-3 mt-sm-0">
@@ -39,7 +46,7 @@ const Sources = () => {
           <SearchWithButton />
         </div>
 
-        <div className="d-flex flex-column flex-sm-row mt-0 mt-md-4 back-supplier-parent">
+        <div className="d-flex flex-column mt-0 mt-md-3 back-supplier-parent">
           <h2
             className="back-to-supplier-heading d-flex align-items-center"
             onClick={() => initialStateSourceSettings()}
@@ -50,23 +57,45 @@ const Sources = () => {
             Back to suppliers overview
           </h2>
 
-          <div className="beta-bg ml-0 ml-sm-4 d-none d-md-flex">
+          <h2 className="auto-ordering-heading-text px-3">
+            Source:{' '}
+            <span>
+              {' '}
+              {whatSelect && whatSelect !== 'Select Supplier' ? whatSelect : 'select a supplier from the list'}{' '}
+            </span>
+          </h2>
+          {/* <div className="beta-bg ml-0 ml-sm-4 d-none d-md-flex">
             <h2 className="mb-0 mr-2">Beta:</h2>
             <span>This service is free while in beta</span>
-          </div>
+          </div> */}
         </div>
 
         <div className={` ${showOrdering ? '' : 'h-100'} auto-ordering-section my-0 my-md-3`}>
-          <h2 className="auto-ordering-heading-text mb-3 ">
+          {/* <h2 className="auto-ordering-heading-text mb-3 ">
             Autoordering supplier configuration:{' '}
             <span> {whatSelect ? whatSelect : 'select a supplier from the list'} </span>
-          </h2>
+          </h2> */}
+          <div className="d-flex justify-content-between mb-4">
+            <div className="d-flex">
+              <div className="enable-disable-para ">
+                <p>{t('SourceConfigInputs.EnableDisableAutoOrdering')}</p>
+                <span className="mr-5">Disabling auto-ordering will require you to manually process new orders.</span>
+              </div>
 
-          <div className="supplier-dropdown">
-            <SearchSelect whatSelect={whatSelect} setWhatSelect={setWhatSelect} setShowOrdering={setShowOrdering} />
+              <div className="custom-control  d-flex align-items-center switchbox custom-switch px-2">
+                <label className="switch-toggle mb-0 " htmlFor="checkbox-2">
+                  <input className="input-toggle-switch" onChange={handleChange} type="checkbox" id="checkbox-2" />
+                  <div className="slider-toggle round"></div>
+                </label>
+              </div>
+            </div>
+
+            <div className="supplier-dropdown">
+              <SearchSelect whatSelect={whatSelect} setWhatSelect={setWhatSelect} setShowOrdering={setShowOrdering} />
+            </div>
           </div>
 
-          {showOrdering ? <AutoOrdering whatSelect={whatSelect} /> : ''}
+          {showOrdering ? <AutoOrdering checked={checked} whatSelect={whatSelect} /> : ''}
         </div>
       </div>
     </>
