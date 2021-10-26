c1ee5051b641fbe64d5c754b7f5d92c6ade344b3
sharukhk08 <iamsharukh08@gmail.com>
2021-10-26 16:20:15 +0530

    change source setting text

--- src/components/sources/SourceSettingsListItem.tsx
+++ src/components/sources/SourceSettingsListItem.tsx
@@ -8,7 +8,7 @@ export const SourceSettingsListItem = () => {
       <div className="row mb-0 mb-sm-3 justify-content-between">
         <div className="col-12 col-xl-6">
           <div className="source-setting-list-item">
-            <h4>Price descrease</h4>
+            <h4>Markup %</h4>
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id vero aliquid autem quisquam consequuntur
               libero facilis a. Sed asperiores adipisci, magnam neque blanditiis eaque, perferendis non cum pariatur
@@ -24,7 +24,7 @@ export const SourceSettingsListItem = () => {
       <div className="row mb-0 mb-sm-3 justify-content-between">
         <div className="col-12 col-xl-6">
           <div className="source-setting-list-item">
-            <h4>Price descrease</h4>
+            <h4>Default template</h4>
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id vero aliquid autem quisquam consequuntur
               libero facilis a. Sed asperiores adipisci, magnam neque blanditiis eaque, perferendis non cum pariatur
@@ -40,7 +40,7 @@ export const SourceSettingsListItem = () => {
       <div className="row mb-0 mb-sm-3 justify-content-between">
         <div className="col-12 col-xl-6">
           <div className="source-setting-list-item">
-            <h4>Price descrease</h4>
+            <h4>Monitor stock</h4>
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id vero aliquid autem quisquam consequuntur
               libero facilis a. Sed asperiores adipisci, magnam neque blanditiis eaque, perferendis non cum pariatur
@@ -56,7 +56,7 @@ export const SourceSettingsListItem = () => {
       <div className="row mb-0 mb-sm-3 justify-content-between">
         <div className="col-12 col-xl-6">
           <div className="source-setting-list-item">
-            <h4>Price descrease</h4>
+            <h4>Monitor price</h4>
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id vero aliquid autem quisquam consequuntur
               libero facilis a. Sed asperiores adipisci, magnam neque blanditiis eaque, perferendis non cum pariatur
