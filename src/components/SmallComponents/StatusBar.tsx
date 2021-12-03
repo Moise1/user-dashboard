a2c25d4432183a60353fcdfb3d86ee3c249b8eab
Moise1 <rwibutso.mo@gmail.com>
2021-12-03 15:13:26 +0200

    add icons to the table

--- src/components/SmallComponents/StatusBar.tsx
+++ src/components/SmallComponents/StatusBar.tsx
@@ -1,10 +1,10 @@
 import '../../sass/light-theme/listings-status.scss';
-
 interface StatusBarProps {
   children: JSX.Element[];
+  className?: string;
 }
 const StatusBar = (props: StatusBarProps) => {
-  return <div className="status-bar">{props.children}</div>;
+  return <div className={`status-bar ${props.className}`}>{props.children}</div>;
 };
 
 export default StatusBar;
