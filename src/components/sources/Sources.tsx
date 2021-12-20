3a2d7e27d3c3b0a886c8fdfdcae4a4b8bdf16a06
dani-mas <danimascarre@gmail.com>
2021-12-20 13:39:26 +0100

    starting refactor sources.tsx

--- src/components/sources/Sources.tsx
+++ src/components/sources/Sources.tsx
@@ -8,7 +8,9 @@ import SourceSettings from './SourceSettings';
 import SearchSelect from './SearchSelect';
 import SourceButtons from './SourceButtons';
 import '../../sass/light-theme/sources-table.scss';
-import { SearchInput } from '../../components/SmallComponents/SearchBars';
+import { Layout } from 'antd';
+
+import '../../sass/light-theme/sources-settings.scss';
 
 type ContextType = {
   supplierValue: string;
@@ -47,74 +49,70 @@ const Sources = () => {
     }
   };
   return (
-    <div className="sources-table w-100 p-sm-3 ant-layout mt-3 mt-sm-0">
-      <p className="all-the-setting-text d-none d-md-block">
-        {' '}
-        All the settings established under Settings &gt; Sources will prevail over Settings &gt; Channel
-      </p>
+    <Layout className="sources-container">
+      <div className="sources-table w-100 p-sm-3 ant-layout mt-3 mt-sm-0">
+        <div className="d-flex flex-column mt-0 mt-md-3 back-supplier-parent">
+          <h2 className="back-to-supplier" onClick={() => initialStateSourceSettings()}>
+            <span className="back-arrow">
+              <LeftBackArrowIcon />
+            </span>
+            Back to suppliers overview
+          </h2>
 
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <SearchInput />
+          <h1 className="surce-configuration-name">
+            Source:{' '}
+            <span>
+              {' '}
+              {whatSelect && whatSelect !== 'Select Supplier' ? whatSelect : 'select a supplier from the list'}{' '}
+            </span>
+          </h1>
         </div>
-      </div>
-      <div className="d-flex flex-column mt-0 mt-md-3 back-supplier-parent">
-        <h2 className="back-to-supplier-heading d-flex align-items-center" onClick={() => initialStateSourceSettings()}>
-          <span className="left-back-arrow-svg">
-            <LeftBackArrowIcon />
-          </span>
-          Back to suppliers overview
-        </h2>
-
-        <h1 className="auto-ordering-heading-text px-3">
-          Source:{' '}
-          <span>
-            {' '}
-            {whatSelect && whatSelect !== 'Select Supplier' ? whatSelect : 'select a supplier from the list'}{' '}
-          </span>
-        </h1>
-      </div>
-      {/* SOURCE TABS  */}
-      <div className="tab-source">
-        <div
-          className={`${isShowSourceSettings ? 'active-tab-bar' : ' '} tab-list-items`}
-          onClick={() => handleSourceTabs()}
-        >
-          <h4>Sources Settings</h4>
+        {/* SOURCE TABS  */}
+        <div className="tab-source">
+          <div
+            className={`${isShowSourceSettings ? 'active-tab-bar' : ' '} tab-list-items`}
+            onClick={() => handleSourceTabs()}
+          >
+            <h4>Sources Settings</h4>
+          </div>
+          <div
+            className={`${isShowAutoOrderingSetting ? 'active-tab-bar' : ' '} tab-list-items`}
+            onClick={() => handleSourceTabs()}
+          >
+            <h4>Autoordering Settings</h4>
+          </div>
         </div>
-        <div
-          className={`${isShowAutoOrderingSetting ? 'active-tab-bar' : ' '} tab-list-items`}
-          onClick={() => handleSourceTabs()}
-        >
-          <h4>Autoordering Settings</h4>
-        </div>
-      </div>
 
-      <div className="auto-ordering-section">
-        <div className="row justify-content-between flex-column flex-lg-row mt-3">
-          {isShowSourceSettings ? <SourceSettings /> : ''}
-          {isShowAutoOrderingSetting ? (
-            <AutoOrdering
-              showOrdering={showOrdering}
-              whatSelect={whatSelect}
-              setWhatSelect={setWhatSelect}
-              setShowOrdering={setShowOrdering}
-            />
-          ) : (
-            ''
-          )}
+        <div className="auto-ordering-section">
+          <div className="row justify-content-between flex-column flex-lg-row mt-3">
+            {isShowSourceSettings ? <SourceSettings /> : ''}
+            {isShowAutoOrderingSetting ? (
+              <AutoOrdering
+                showOrdering={showOrdering}
+                whatSelect={whatSelect}
+                setWhatSelect={setWhatSelect}
+                setShowOrdering={setShowOrdering}
+              />
+            ) : (
+              ''
+            )}
 
-          <div className="col-12 col-sm-4 mt-2 d-flex flex-column align-items-lg-end ">
-            <div className="sticky-btns">
-              <div className="supplier-dropdown">
-                <SearchSelect whatSelect={whatSelect} setWhatSelect={setWhatSelect} setShowOrdering={setShowOrdering} />
+            <div className="col-12 col-sm-4 mt-2 d-flex flex-column align-items-lg-end ">
+              <div className="sticky-btns">
+                <div className="supplier-dropdown">
+                  <SearchSelect
+                    whatSelect={whatSelect}
+                    setWhatSelect={setWhatSelect}
+                    setShowOrdering={setShowOrdering}
+                  />
+                </div>
+                <SourceButtons />
               </div>
-              <SourceButtons />
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
