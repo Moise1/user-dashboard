2d1e0cc6e95c6d30b01dc49db2f061d24152df1f
dani-mas <danimascarre@gmail.com>
2022-01-03 09:10:26 +0100

    start cleanup new-channel styles

--- src/components/chanel/StoreLocation.tsx
+++ src/components/chanel/StoreLocation.tsx
@@ -13,7 +13,6 @@ import Previousstep from '../small-components/Previousstep';
 import Flag, { FlagProps } from './Flag';
 import '../../sass/light-theme/store-location.scss';
 
-
 interface values {
   storeLocation: string;
 }
@@ -87,48 +86,46 @@ function StoreLocation(props: StoreLocationProps) {
 
   return (
     <form className="location-form">
-      <div className="location-area">
-        <div className="col-xl-8   bg-white  mt-2 pt-1">
-          <Previousstep Previous={Previous} />
-          <div className="row mx-auto px-lg-5 px-md-3 h-resp-65">
-            <div className="text-center col-10 mt-2 mx-auto">
-              <h5 className="font-weight-bold">{t('bar2')}?</h5>
-            </div>
-            <div className="text-center col-12 col-md-10 mx-auto">
-              <h6 className="">
-                {platform == 'ebay' ? t('storebay') : platform == 'amazon' ? t('storamz') : t('storshp')}
-              </h6>
-            </div>
-            {flags.map((i) => (
-              <React.Fragment key={i.name}>
-                <Flag
-                  platform={platform}
-                  currentPlatform={i.platform}
-                  // handleChangeLocation={i.slug}
-                  flag={i.path}
-                  name={i.name}
-                  location={values.storeLocation}
-                  handleChangeLocation={handleChangeLocation}
-                />
-              </React.Fragment>
-            ))}
+      <div className="col-xl-8   bg-white  mt-2 pt-1">
+        <Previousstep Previous={Previous} />
+        <div className="row mx-auto px-lg-5 px-md-3 h-resp-65">
+          <div className="text-center col-10 mt-2 mx-auto">
+            <h5 className="font-weight-bold">{t('bar2')}?</h5>
+          </div>
+          <div className="text-center col-12 col-md-10 mx-auto">
+            <h6 className="">
+              {platform == 'ebay' ? t('storebay') : platform == 'amazon' ? t('storamz') : t('storshp')}
+            </h6>
+          </div>
+          {flags.map((i) => (
+            <React.Fragment key={i.name}>
+              <Flag
+                platform={platform}
+                currentPlatform={i.platform}
+                // handleChangeLocation={i.slug}
+                flag={i.path}
+                name={i.name}
+                location={values.storeLocation}
+                handleChangeLocation={handleChangeLocation}
+              />
+            </React.Fragment>
+          ))}
 
-            <div className="col-12"></div>
-            <div className="mx-md-auto ml-auto mt-md-4 text-right text-md-center w-100 next-fix">
-              <div className="text-danger w-100 text-center small d-block d-md-none">
-                {values.storeLocation == '' ? '  *Please select a country in order to proceed' : ''}
-              </div>
-              <NextBtn onClick={Continue} title={t('nxt')} disabled={values.storeLocation == ''} />
+          <div className="col-12"></div>
+          <div className="mx-md-auto ml-auto mt-md-4 text-right text-md-center w-100 next-fix">
+            <div className="text-danger w-100 text-center small d-block d-md-none">
+              {values.storeLocation == '' ? '  *Please select a country in order to proceed' : ''}
+            </div>
+            <NextBtn onClick={Continue} title={t('nxt')} disabled={values.storeLocation == ''} />
 
-              <div className="text-danger w-100 text-center mb-2 small d-md-block d-none">
-                {values.storeLocation == '' ? t('strchck') : ''}
-              </div>
+            <div className="text-danger w-100 text-center mb-2 small d-md-block d-none">
+              {values.storeLocation == '' ? t('strchck') : ''}
             </div>
           </div>
         </div>
-        <div className="col-xl-4 d-xl-block d-none m-auto">
-          <ProgressBar platform={platform} step={step} />
-        </div>
+      </div>
+      <div className="col-xl-4 d-xl-block d-none m-auto">
+        <ProgressBar platform={platform} step={step} />
       </div>
     </form>
   );
