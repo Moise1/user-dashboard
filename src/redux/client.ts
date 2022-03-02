ef503d1ed421771c46214daddfe8ac196f0b6037
Dani Mas <danimascarre@gmail.com>
2022-03-02 15:23:08 +0100

    cors

--- src/redux/client.ts
+++ src/redux/client.ts
@@ -11,8 +11,25 @@ export const client = axios.create({
 });
 
 client.interceptors.request.use(config =>{
+
+  config.headers = {
+
+    'Access-Control-Allow-Origin': '*',
+
+    'Access-Control-Allow-Methods': '*',
+
+    'Access-Control-Allow-Headers': '*',
+
+    'Accept': 'application/json',
+
+    'Content-Type': 'application/json'
+
+  };
+
   return config;
+
 },
+
 (error) => Promise.reject(error)
 );
 
