3bf0fe2ccb5294024305037b1a0978fca84e9358
Moise1 <rwibutso.mo@gmail.com>
2022-03-11 23:11:36 +0200

    refactor code again

--- src/redux/channels/channelsThunk.ts
+++ src/redux/channels/channelsThunk.ts
@@ -5,10 +5,10 @@ import { client } from '../client';
 export const getChannels = createAsyncThunk(
   'channels/getChannels',
   async (_, {rejectWithValue} /* destructured thunkAPI's prop */) => {
-  try {
-    const res = await client.get('/User/Channels/Get/');
-    return res.data.response_data.channels;
-  } catch (error) {
-    return rejectWithValue('Sorry! Something went wrong ):') ;
-  }
-});
+    try {
+      const res = await client.get('/User/Channels/Get/');
+      return res.data.response_data.channels;
+    } catch (error) {
+      return rejectWithValue('Sorry! Something went wrong ):') ;
+    }
+  });
