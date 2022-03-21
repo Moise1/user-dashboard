e4d8097b970ea5738bcfe1f4322c8a98aa4832a8
Moise1 <rwibutso.mo@gmail.com>
2022-03-21 12:07:27 +0200

    edit channel id

--- src/redux/client.ts
+++ src/redux/client.ts
@@ -10,34 +10,35 @@ export const client = axios.create({
   validateStatus: (status) => (status >= 200 && status <= 404) || status <= 500
 });
 
-client.interceptors.request.use(async(req: AxiosRequestConfig) =>{
-  const channelId = localStorage.getItem('channelId');
-  
-  if(channelId){
+client.interceptors.request.use(
+  async (req: AxiosRequestConfig) => {
+    const channelId = localStorage.getItem('channelId');
+
     req.headers = {
-      channel:  channelId,
+      channel: channelId || 1,
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': '*',
       'Access-Control-Allow-Headers': '*',
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
-      ...req.headers,
+      ...req.headers
     };
+    return req;
+  },
 
-  }
-  return req;
-},
-
-(error) => Promise.reject(error)
+  (error) => Promise.reject(error)
 );
 
-client.interceptors.response.use(res =>{
-  if(res.status === 500){
-    toastAlert(res.statusText, 'error');
-  }else if(res.status === 404){
-    toastAlert(res.data.response_errors.error, 'error');
-  }else if(res.status === 409){
-    toastAlert(res.data.response_errors.error[0].description, 'error');
-  } 
-  return res;
-}, (error) => Promise.reject(error)); 
+client.interceptors.response.use(
+  (res) => {
+    if (res.status === 500) {
+      toastAlert(res.statusText, 'error');
+    } else if (res.status === 404) {
+      toastAlert(res.data.response_errors.error, 'error');
+    } else if (res.status === 409) {
+      toastAlert(res.data.response_errors.error[0].description, 'error');
+    }
+    return res;
+  },
+  (error) => Promise.reject(error)
+);
