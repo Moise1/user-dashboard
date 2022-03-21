1e4205b7fa064aa950a6e5c2aabf46463f37e510
Nabeel Iqbal <nabeel.exp@gmail.com>
2022-03-21 16:18:34 +0500

    virtual assitant issue fix

--- src/redux/va-profiles/vaProfilesThunk.ts
+++ src/redux/va-profiles/vaProfilesThunk.ts
@@ -3,8 +3,8 @@ import { client } from '../client';
 
 export const getUserAssistants = createAsyncThunk('userAssistants/getUserAssistants', async (_, thunkAPI) => {
   try {
-    const res = await client.post('/User/VirtualAssistant/Get');
-    return res.data;
+    const res = await client.get('/VirtualAssistant/Get');
+    return res.data.response_data;
   } catch (error) {
     return thunkAPI.rejectWithValue('Sorry! Something went wrong ):') ;
   }
