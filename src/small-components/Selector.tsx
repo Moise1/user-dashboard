61e54bc457c9af5e0634e52af78209b9670889e3
Moise1 <rwibutso.mo@gmail.com>
2022-04-21 10:51:26 +0200

    show flags

--- src/small-components/Selector.tsx
+++ src/small-components/Selector.tsx
@@ -2,11 +2,13 @@ import { ReactElement, JSXElementConstructor, ReactNode, CSSProperties } from 'r
 import { Select } from 'antd';
 import { Rule } from '../redux/pricing-rules/rulesSlice';
 import { SourceConfig } from '../redux/source-config/sourceSlice';
+import { Channel } from '../redux/channels/channelsSlice';
 import { DummyData } from '../dummy-data/dummyData';
+import {countryFlag} from '../utils/countryFlag';
 import '../sass/selector.scss';
 
 interface Props {
-  children: Array<DummyData | Rule | SourceConfig>;
+  children: Array<DummyData | Rule | SourceConfig | Channel>;
   defaultValue?: string;
   addAccount?: boolean;
   onChange?: (value: string) => void;
@@ -17,15 +19,20 @@ interface Props {
   ) => ReactElement<ReactNode, string | JSXElementConstructor<ReactNode>>;
   loading?: boolean;
   style?: CSSProperties;
+  showFlags?: boolean;
 }
 
 const { Option } = Select;
 type sizeType = 'large' | 'small' | 'middle';
+
 export const Selector: React.FC<Props> = (props: Props) => {
-  const { children, defaultValue, onChange, dropdownRender, loading, style, size } = props;
+  const { children, defaultValue, onChange, dropdownRender, loading, style, size, showFlags } = props;
+
+  
   const options = children?.map((c) => {
     return (
       <Option key={c.id} value={c.value}>
+        {showFlags && countryFlag(c.isoCountry)}
         {c.value}
       </Option>
     );
