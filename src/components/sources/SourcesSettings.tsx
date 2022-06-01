38c7d9f7b0b48655caa2d4a3039d10ca9fc36cc9
bewaleed <105792053+bewaleed@users.noreply.github.com>
2022-06-01 12:02:44 +0500

    Source Setting Save

--- src/components/sources/SourcesSettings.tsx
+++ src/components/sources/SourcesSettings.tsx
@@ -5,12 +5,13 @@ import { ChevronLeft } from 'react-feather';
 // import AutoOrdering from '../auto-ordering/AutoOrdering';
 //import { /*SourcesSettingsContents*/ } from './SourcesSettingsContents';
 import { t } from '../../utils/transShim';
-import { SuccessBtn } from '../../small-components/ActionBtns';
+//import { SuccessBtn } from '../../small-components/ActionBtns';
 import { CountrySelector } from '../../small-components/CountrySelector';
 import { SimpleSelect } from '../../small-components/SimpleSelect';
 import { Switch } from '../../small-components/Switch';
 import { useAppDispatch, useAppSelector } from 'src/custom-hooks/reduxCustomHooks';
-import { getSources } from '../../redux/source-config/sourcesThunk';
+import { getSources, saveSources } from '../../redux/source-config/sourcesThunk';
+import { SourceConfigSave } from '../../redux/source-config/sourceSlice';
 //import { SourceConfig } from '../../redux/source-config/sourceSlice';
 import '../../sass/sources-settings.scss';
 
@@ -27,18 +28,22 @@ export const SourcesSettings = () => {
   //const [source, setSource] = useState();
   const { sources, sourcesLoading } = useAppSelector((state) => state.sources);
   // Source Fields
+  const [userId, setUserId] = useState<string>();
+  const [sourceId, setSourceId] = useState<number>();
+  const [sourceName, setSourceName] = useState<string>();
   const [markup, setMarkup] = useState<number | null | undefined>();
   const [markupSelect, setMarkupSelect] = useState<string | null | undefined>();
-  const [templateId, setTemplateId] = useState<string | null | undefined>();
-  const [monitorStock, setMonitorStock] = useState<string | null | undefined>();
-  const [monitorPrice, setMonitorPrice] = useState<string | null | undefined>();
-  const [monitorPriceDecrease, setMonitorPriceDecrease] = useState<string | null | undefined>();
-  const [monitorPriceDecreasePercentage, setmonitorPriceDecreasePercentage] = useState<number | null | undefined>();
+  const [templateId, setTemplateId] = useState<number | null | undefined>();
+  const [monitorStock, setMonitorStock] = useState<boolean | null | undefined>();
+  const [monitorPrice, setMonitorPrice] = useState<boolean | null | undefined>();
+  const [monitorPriceDecrease, setMonitorPriceDecrease] = useState<boolean | null | undefined>();
+  const [monitorPriceDecreasePercentage, setmonitorPriceDecreasePercentage] = useState<number | undefined>();
   const [returns, setReturns] = useState<string | null | undefined>();
-  const [dispatchDays, setDispatchDays] = useState<number | null | undefined>();
-  const [globalShippingProgram, setGlobalShippingProgram] = useState<boolean | null | undefined>();
+  const [dispatchDays, setDispatchDays] = useState<number | null>();
+  const [globalShippingProgram, setGlobalShippingProgram] = useState<boolean | null>(false);
   const [defaultLocationPostcode, setDefaultLocationPostcode] = useState<string | null | undefined>();
   const [defaultLocationCity, setDefaultLocationCity] = useState<string | null | undefined>();
+  const [defaultLocationCountry, setDefaultLocationCountry] = useState<string | null | undefined>();
   const [maxDeliveryDays, setMaxDeliveryDays] = useState<number | null | undefined>();
   const [primeOnly, setPrimeOnly] = useState<boolean | null | undefined>();
 
@@ -91,9 +96,11 @@ export const SourcesSettings = () => {
   };
 
   const [hideMarkupInput, setHideMarkupInput] = useState(true);
+  const [selectLimit, setSelectLimit] = useState(true);
   const [hideMonitorStock, setHideMonitorStock] = useState(false);
   const [hidePriceDecrease, setHidePriceDecrease] = useState(false);
   const [hideCustomPrice, setHideCustomPrice] = useState(true);
+  const [customPriceDecreaseDRD, setCustomPriceDecreaseDRD] = useState('custom');
   const handleMarkupChange = (value: string) => {
     setMarkupSelect(value);
     if (value === 'custom') {
@@ -104,7 +111,7 @@ export const SourcesSettings = () => {
     }
   };
   const handleTemplateChange = (value: string) => {
-    setTemplateId(value);
+    setTemplateId(Number(value));
   };
 
 
@@ -125,7 +132,7 @@ export const SourcesSettings = () => {
     setMonitorPriceDecrease(null);
 
     // monitor price decrease Percentage
-    setmonitorPriceDecreasePercentage(null);
+    setmonitorPriceDecreasePercentage(undefined);
 
     // return days
     setReturns(null);
@@ -143,6 +150,9 @@ export const SourcesSettings = () => {
     setDefaultLocationCity(null);
 
     // Default Location City
+    setDefaultLocationCountry(null);
+
+    // Default Location City
     setMaxDeliveryDays(null);
 
     // Default Prime Only 
@@ -150,10 +160,10 @@ export const SourcesSettings = () => {
 
   }
 
-
   function loadSource(selectedSource: string | null) {
     sources.map((c: {
-      sourceId: Key | null | undefined;
+      userId: string | undefined;
+      sourceId: number | null | undefined;
       sourceName: string;
       markup: number | undefined;
       templateId: number | undefined;
@@ -163,14 +173,21 @@ export const SourcesSettings = () => {
       monitorPriceDecreasePercentage: number | undefined;
       returns: string | undefined;
       dispatchDays: number | undefined;
-      globalShippingProgram: boolean | undefined;
+      globalShippingProgram: boolean;
       defaultLocationPostcode: string | undefined;
       defaultLocationCity: string | undefined;
+      defaultLocationCountry: string | undefined;
       maxDeliveryDays: number | undefined;
       primeOnly: boolean | undefined;
     }) => {
 
-      if (c.sourceId === selectedSource || c.sourceName === selectedSource) {
+      if (c.sourceId === Number(selectedSource) || c.sourceName === selectedSource) {
+
+        setSourceId(Number(c.sourceId));
+
+        setUserId(c.userId);
+
+        setSourceName(c.sourceName);
 
         //markup business logic implements
         setMarkup(c.markup ? c.markup : undefined);
@@ -178,19 +195,23 @@ export const SourcesSettings = () => {
         setHideMarkupInput(c.markup ? false : true);
 
         // selected template
-        setTemplateId(c.templateId ? c.templateId.toString() : 'Defined by Settings(Plain)');
+        setTemplateId(c.templateId ? c.templateId : undefined);
 
         // monitor stock
-        setMonitorStock(c.monitorStock?.toString());
+        setMonitorStock(c.monitorStock);
 
         // monitor price
-        setMonitorPrice(c.monitorPrice?.toString());
+        setMonitorPrice(c.monitorPrice);
 
         // monitor price decrease
-        setMonitorPriceDecrease(c.monitorPriceDecrease?.toString());
-
+        setMonitorPriceDecrease(c.monitorPriceDecrease);
+        
         // monitor price decrease Percentage
         setmonitorPriceDecreasePercentage(c.monitorPriceDecreasePercentage);
+        if (c.monitorPriceDecreasePercentage) {
+          setHideCustomPrice(false);
+          setSelectLimit(true);
+        }
 
         // return days
         setReturns(c.returns?.toString());
@@ -207,7 +228,10 @@ export const SourcesSettings = () => {
         // Default Location City
         setDefaultLocationCity(c.defaultLocationCity);
 
-        // Default Location City
+        // Default Location Country
+        setDefaultLocationCountry(c.defaultLocationCountry);
+
+        // Max Delivery Days
         setMaxDeliveryDays(c.maxDeliveryDays);
 
         // Default Prime Only 
@@ -217,6 +241,43 @@ export const SourcesSettings = () => {
       }
     });
   }
+
+  const onSave = async (values: SourceConfigSave[]) => {
+    await dispatch(
+      saveSources(values)
+    );
+    dispatch(getSources());
+  };
+
+  function save() {
+    //if (primeOnly) {
+
+    //}
+    //else {
+
+    //}
+    const value: SourceConfigSave[] = [{
+      sourceId: sourceId ? sourceId : 0,
+      userId: userId ? userId : '',
+      sourceName: sourceName ? sourceName : '',
+      markup: markup ? markup : undefined,
+      monitorPrice: monitorPrice ? monitorPrice : undefined,
+      monitorPriceDecrease: monitorPriceDecrease ? monitorPriceDecrease : undefined,
+      monitorPriceDecreasePercentage: monitorPriceDecreasePercentage,
+      monitorStock: monitorStock ? monitorStock : undefined,
+      dispatchDays: dispatchDays ? dispatchDays : undefined,
+      defaultLocationPostcode: defaultLocationPostcode ? defaultLocationPostcode : '',
+      maxDeliveryDays: maxDeliveryDays ? maxDeliveryDays : undefined,
+      returns: returns ? returns : '',
+      templateId: templateId ? templateId : undefined,
+      defaultLocationCity: defaultLocationCity ? defaultLocationCity : '',
+      defaultLocationCountry: defaultLocationCountry ? defaultLocationCountry : '',
+      defaultShipping: ''
+    }];
+
+    onSave(value);
+  }
+
 
   const handleOptionChange = (value: string) => {
     loadSource(value);
@@ -233,15 +294,16 @@ export const SourcesSettings = () => {
   const handleMonitorStock = (value: string) => {
     if (value === 'false') {
       setHideMonitorStock(true);
+      setMonitorStock(false);
     }
     else {
       setHideMonitorStock(false);
+      setMonitorStock(true);
     }
-    setMonitorStock(value);
   };
 
   const handlMonitorPrice = (value: string) => {
-    setMonitorPrice(value);
+    setMonitorPrice(Boolean(value));
   };
 
   const handlePriceChange = (value: string) => {
@@ -251,10 +313,13 @@ export const SourcesSettings = () => {
     else {
       setHidePriceDecrease(false);
     }
-    setMonitorPriceDecrease(value);
+    setMonitorPriceDecrease(Boolean(value));
   };
 
   const handleCustomPriceChange = (value: string) => {
+
+    console.log('handleCustomPriceChange: ' + value);
+    setCustomPriceDecreaseDRD(value);
     if (value === 'custom') {
       setHideCustomPrice(false);
     }
@@ -281,6 +346,10 @@ export const SourcesSettings = () => {
 
   const handleDefaultLocationCity = (event: React.ChangeEvent<HTMLInputElement>) => {
     setDefaultLocationCity(event.target.value);
+  };
+
+  const handleDefaultLocationCountry = (value: string) => {
+    setDefaultLocationCountry(value);
   };
 
   const handleMaxDeliveryDays = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -346,7 +415,7 @@ export const SourcesSettings = () => {
             </p>
           </Col>
           <Col className="selector-container" xs={7} lg={6}>
-            <SimpleSelect value={templateId ? templateId : ''} onChange={handleTemplateChange}>{TempOptions}</SimpleSelect>
+            <SimpleSelect value={templateId ? templateId.toString() : undefined} onChange={handleTemplateChange}>{TempOptions}</SimpleSelect>
           </Col>
         </Row>
 
@@ -359,7 +428,7 @@ export const SourcesSettings = () => {
             </p>
           </Col>
           <Col className="selector-container" xs={7} lg={6}>
-            <SimpleSelect defaultValue="Defined by Settings(yes)" onChange={handleMonitorStock} value={monitorStock ? monitorStock : 'Defined by Settings(Yes)'}>
+            <SimpleSelect defaultValue="Defined by Settings(yes)" onChange={handleMonitorStock} value={monitorStock ? 'true' : 'Defined by Settings(Yes)'}>
               <Option key="0">Defined by Settings(Yes)</Option>
               <Option value="true" key="1">Yes</Option>
               <Option value="false" key="2">No</Option>
@@ -376,7 +445,7 @@ export const SourcesSettings = () => {
             </p>
           </Col>
           <Col className="selector-container" xs={7} lg={6}>
-            <SimpleSelect defaultValue="Defined by Settings(yes)" value={monitorPrice ? monitorPrice : 'Defined by Settings(Yes)'} onChange={handlMonitorPrice}>
+            <SimpleSelect defaultValue="Defined by Settings(yes)" value={monitorPrice ? 'true' : 'Defined by Settings(Yes)'} onChange={handlMonitorPrice}>
               <Option key="0">Defined by Settings(Yes)</Option>
               <Option value="true" key="1">Yes</Option>
               <Option value="false" key="2">No</Option>
@@ -405,14 +474,14 @@ export const SourcesSettings = () => {
           </Col>
           <Col className="selector-container" xs={7} lg={6}>
             <br />
-            <SimpleSelect defaultValue="Defined by Settings(yes)" onChange={handlePriceChange} value={monitorPriceDecrease ? monitorPriceDecrease : 'Defined by Settings(yes)'}>
+            <SimpleSelect defaultValue="Defined by Settings(yes)" onChange={handlePriceChange} value={monitorPriceDecrease ? 'true' : 'Defined by Settings(yes)'}>
               <Option key="0">Defined by Settings(Yes)</Option>
               <Option value="true" key="1">Yes</Option>
               <Option value="false" key="2">No</Option>
             </SimpleSelect>
             <div className="divPadding" hidden={hidePriceDecrease}>
-              <SimpleSelect defaultValue="Defined by Settings(0)" onChange={handleCustomPriceChange} value={limitValue ? 'custom' : ''}>
-                <Option key="0">Defined by Settings(0)</Option>
+              <SimpleSelect defaultValue="Defined by Settings(0)" onChange={handleCustomPriceChange} value={customPriceDecreaseDRD ? customPriceDecreaseDRD : ''}>
+                <Option key="0" value="default">Defined by Settings(0)</Option>
                 <Option value="custom" key="1">Custom</Option>
               </SimpleSelect>
             </div>
@@ -422,7 +491,7 @@ export const SourcesSettings = () => {
                 <br />
                 <div id="container">
                   <div id="left">
-                    <Radio value="Limit" key="2">Limit % </Radio>
+                    <Radio value="Limit" key="2" checked={selectLimit}>Limit % </Radio>
                   </div>
                   <div id="right">
                     <Input disabled={disableRadioLimit} type="number" size="small" value={monitorPriceDecreasePercentage ? monitorPriceDecreasePercentage : undefined} onChange={handleMonitorPriceDecreasePercentage} />
@@ -462,7 +531,7 @@ export const SourcesSettings = () => {
           </Col>
           <Col className="selector-container" xs={7} lg={6}>
             <div className="input-control" >
-              <Input type="number" value={dispatchDays ? dispatchDays: undefined} onChange={handleDispatchDays} />
+              <Input type="number" value={dispatchDays ? dispatchDays : undefined} onChange={handleDispatchDays} />
             </div>
           </Col>
         </Row>
@@ -495,7 +564,7 @@ export const SourcesSettings = () => {
           </Col>
           <Col className="selector-container" xs={7} lg={6}>
             <div className="input-control" >
-              <Input type="text" value={defaultLocationCity ? defaultLocationCity: undefined} onChange={handleDefaultLocationCity} />
+              <Input type="text" value={defaultLocationCity ? defaultLocationCity : undefined} onChange={handleDefaultLocationCity} />
             </div>
           </Col>
         </Row>
@@ -505,7 +574,7 @@ export const SourcesSettings = () => {
             <h2>Overwrite Item Country Code</h2>
           </Col>
           <Col className="selector-container" xs={7} lg={6}>
-            <CountrySelector defaultValue="Default"></CountrySelector>
+            <CountrySelector defaultValue="Default" value={defaultLocationCountry ? defaultLocationCountry : 'Default'} onChange={handleDefaultLocationCountry}></CountrySelector>
           </Col>
         </Row>
 
@@ -518,7 +587,7 @@ export const SourcesSettings = () => {
           </Col>
           <Col className="selector-container" xs={7} lg={6}>
             <div className="input-control" >
-              <Input type="text" value={maxDeliveryDays ? maxDeliveryDays: undefined} onChange={handleMaxDeliveryDays} />
+              <Input type="text" value={maxDeliveryDays ? maxDeliveryDays : undefined} onChange={handleMaxDeliveryDays} />
             </div>
           </Col>
         </Row>
@@ -540,7 +609,7 @@ export const SourcesSettings = () => {
           {sourceOptions}
         </SimpleSelect>
         <div className="action-btns">
-          <SuccessBtn>{t('SaveChanges')}</SuccessBtn>
+          <Button onClick={save}>{t('SaveChanges')}</Button>
           <Button onClick={loadDefault}>{t('ResetToDefault')}</Button>
         </div>
       </div>
