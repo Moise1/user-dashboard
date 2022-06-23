176677f0dc81c1a7ae9b449b4f8b081e51592f3e
Dani Mas <danimascarre@gmail.com>
2022-06-23 10:05:57 +0200

    listing services UI

--- src/components/listing-service/ConfigureListingService.tsx
+++ src/components/listing-service/ConfigureListingService.tsx
@@ -5,18 +5,22 @@ import '../../sass/listing-service/configure-listing-service.scss';
 import { Channel } from 'src/redux/channels/channelsSlice';
 
 import { Selector } from 'src/small-components/form/selector';
-import { Divider, Radio, Spin } from 'antd';
+import { Divider, Input, Radio, Spin } from 'antd';
 import { SimpleTable } from 'src/small-components/simple-table';
 import { CrossModalIcon } from '../common/Icons';
+import { useState } from 'react';
 
 export const ConfigureListingService = () => {
   const { channels }: { channels: Channel[] } = useAppSelector((state) => state.channels);
+
+  const [selectedChannel] = useState('teststore');
 
   const { listingServicesResult, loading } = useAppSelector((state) => state.listingServices);
 
   const { sources } = useAppSelector((state) => state.sourcesReducer);
 
   console.log(sources);
+  console.log(listingServicesResult);
 
   const noSuscribed = (
     <div className="nosuscribed-container">
@@ -78,14 +82,6 @@ export const ConfigureListingService = () => {
       }
     },
     {
-      title: 'ACtions',
-      dataIndex: '',
-      key: '',
-      render: () => {
-        return <ConfirmBtn>Start listing</ConfirmBtn>;
-      }
-    },
-    {
       title: 'Date',
       dataIndex: '',
       key: '',
@@ -122,7 +118,9 @@ export const ConfigureListingService = () => {
         <div className="configuration-section">
           {listingServicesResult?.length ? (
             <div className="listingservice-configuration">
-              <h3>Channel name</h3>
+              <h3>
+                Configure {listingServicesResult.quantity} listing service for the account: {selectedChannel}
+              </h3>
               <div className="sources-options">
                 <div className="sources">
                   <div className="included-sources">
@@ -157,13 +155,66 @@ export const ConfigureListingService = () => {
               <div className="price-options">
                 <Radio>Source price preference</Radio>
                 <Radio>Profit preference</Radio>
+                <div className="inputs-container">
+                  <label>Source price</label>
+                  <div className="inputs">
+                    <Input placeholder="Min" className="blue-input" />
+                    <Input placeholder="Max" className="blue-input" />
+                  </div>
+                </div>
               </div>
+              <ConfirmBtn className="list">Start listing service with selected preferences</ConfirmBtn>
             </div>
           ) : (
             noSuscribed
           )}
         </div>
       )}
+      <div className="explanation-section">
+        <h2>Is your store ready for us to start listing?</h2>
+        <p>
+          <strong>If you are selling on eBay:</strong>
+        </p>
+        <p>
+          Make sure your billing information is completed. eBay requires you to set up a billing method with them before
+          they enable your seller account. In order to do so, you can do the following:
+        </p>
+        <p>
+          Access your eBay account and introduce your billing details (AU, DE, ES, FR, IT, USA, UK). If you are not able
+          to find the section, please contact eBay via the chat on their site and request them to send you the link to
+          complete your billing details.
+        </p>
+        <p>
+          <strong>Your listing settings</strong>
+        </p>
+
+        <p>
+          To make sure we list according to your preferences, please access your Hustle Got Real account and configure
+          your channel settings.
+        </p>
+        <h2>Your listing preferences</h2>
+        <p>
+          <strong>Profit preferences</strong>
+        </p>
+        <p>
+          Once you have purchased the service, you will be able to select your profit and suppliers preferences. Please
+          note that when we calculate the profit we are referring to the net profit you will have once the item is sold.
+          (already considering PayPal and eBay or Amazon estimated fees).
+        </p>
+        <p>Profit = buying price - selling price - fees</p>
+        <p>
+          If you choose a high minimum profit (higher than 3) your items will be much more expensive and therefore
+          harder to sell. We recommend that option for more expert drop shippers.
+        </p>
+        <p>
+          <strong>Suppliers preferences</strong>
+        </p>
+        <p>
+          The important thing to consider when choosing the suppliers is shipping time. Please note that this will vary
+          from one supplier to another. Some suppliers also need you to have a dropshipping account to get started, so
+          please bear that in mind when setting up your preferences.
+        </p>
+      </div>
     </div>
   );
 };
