75e172c805c50336e0955673131fcba91a5e8c99
Dani Mas <danimascarre@gmail.com>
2022-06-23 11:33:01 +0200

    start price warrior and remove padding selector

--- src/components/services/PriceWarrior.tsx
+++ src/components/services/PriceWarrior.tsx
@@ -1,73 +1,45 @@
-import { Link } from 'react-router-dom';
-import { Row, Col } from 'antd';
+import { Form } from 'antd';
 import '../../sass/services/service.scss';
-import { LeftOutlined } from '@ant-design/icons';
-import { Links } from '../../links';
-import imgpricewarrior from '../../assets/services/price_warrior.png';
+import { Selector } from 'src/small-components/form/selector';
+import { useAppSelector } from 'src/custom-hooks/reduxCustomHooks';
+import { Channel } from 'src/redux/channels/channelsSlice';
+import '../../sass/services/price-warrior-configuration.scss';
+
+const { Item } = Form;
 
 export const PriceWarrior = () => {
-  return (
-    <div className="main-container">
-      <Link to={Links.Services} className="back-to-services">
-        <a>
-          <LeftOutlined style={{ fontSize: '19px' }} />
-          Back to services
-        </a>
-      </Link>
-      <div className="service-main-container">
-        <Row className="service-area">
-          <Col className="col-services" xs={24} md={24} lg={8}>
-            <div className="service-container">
-              <div className="image-container">
-                <img src={imgpricewarrior}></img>
-              </div>
-            </div>
-          </Col>
+  const { channels }: { channels: Channel[] } = useAppSelector((state) => state.channels);
 
-          <Col xs={24} lg={12}>
-            <div className="description-area">
-              <h2 className="service-title">Price Warrior</h2>
-              <div className="service-advantages">
-                <p>
-                  Price Warrior monitors all your listings every day to detect thieves. It undercuts thieves by a set
-                  amount automatically.
-                </p>
-                <ul>
-                  <li>
-                    Define a minimum markup you are willing to sell at. Price Warrior will never set the price below
-                    that markup.
-                  </li>
-                  <li>
-                    Price Warrior will update automatically, and you will have access to a report showing the status of
-                    all the listings being managed by Price Warrior.{' '}
-                  </li>
-                </ul>
-              </div>
-              <div className="service-pricing-container">
-                <div className="service-pricing-table">
-                  <div className="service-cost">
-                    <h4 className="cost-title">Cost of this service</h4>
-                  </div>
-                  <div className="service-cost-details">
-                    <div className="service-rate-container">
-                      <div className="rate-details">
-                        <span className="euro">€</span>
-                        <h1 className="monthly-rate">50</h1>
-                        <h4 className="frequency">/mo</h4>
-                      </div>
-                      <div className="type-payment">
-                        <h4>Manteinance fee</h4>
-                      </div>
-                      <div className="what-includes">
-                        <p>To ensure that the integration keeps working even if your supplier changes the website.</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Col>
-        </Row>
+  return (
+    <div className="price-warrior-container">
+      <div className="options-container">
+        <Form className="options-form" layout={'vertical'}>
+          <Item label="Price Warrior">
+            <Selector placeHolder="Select a channel">
+              {channels?.map(({ name: label, id: value }: Channel) => ({ value, label }))}
+            </Selector>
+          </Item>
+          <Item label="Repricing">
+            <Selector placeHolder="Select a channel">
+              {channels?.map(({ name: label, id: value }: Channel) => ({ value, label }))}
+            </Selector>
+          </Item>
+          <Item label="Minimum Markup (%)">
+            <Selector placeHolder="Select a channel">
+              {channels?.map(({ name: label, id: value }: Channel) => ({ value, label }))}
+            </Selector>
+          </Item>
+          <Item label="Undercut by">
+            <Selector placeHolder="Select a channel">
+              {channels?.map(({ name: label, id: value }: Channel) => ({ value, label }))}
+            </Selector>
+          </Item>
+          <Item label="Treshold">
+            <Selector placeHolder="Select a channel">
+              {channels?.map(({ name: label, id: value }: Channel) => ({ value, label }))}
+            </Selector>
+          </Item>
+        </Form>
       </div>
     </div>
   );
--- src/links.tsx
+++ src/links.tsx
@@ -25,6 +25,7 @@
   ConfigureNoapi: '/configure-no-api-server',
   Login: '/login',
   Register: '/register',
+  PriceWarrior: '/price-warrior',
   ListingService: 'https://hustlegotreal.com/en/listing-service/',
   AutoOrderConfigurationQuery: '/auto-ordering-configuration-query' //<--What's this?
 };
