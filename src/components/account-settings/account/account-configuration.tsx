116c6220c5b407447e8d1d045510d99165e3c396
Dani Mas <danimascarre@gmail.com>
2022-06-27 09:40:17 +0200

    ui fixes

--- src/components/account-settings/account/account-configuration.tsx
+++ src/components/account-settings/account/account-configuration.tsx
@@ -4,9 +4,18 @@ import { StatusBar } from '../../../small-components/StatusBar';
 import { StatusBtn } from '../../../small-components/StatusBtn';
 import { t, TransUtils } from '../../../utils/transShim';
 import '../../../sass/account-settings.scss';
+import '../../../sass/channel-settings.scss';
 import { useAppDispatch, useAppSelector } from '../../../custom-hooks/reduxCustomHooks';
-import { getAccountConfiguration, saveAccountSetting } from '../../../redux/account-configuration/account-configuration-thunk';
-import { Account, AccountConfigurationState, eAccountSettings, SettingsValue } from '../../../redux/account-configuration/account-configuration-slice';
+import {
+  getAccountConfiguration,
+  saveAccountSetting
+} from '../../../redux/account-configuration/account-configuration-thunk';
+import {
+  Account,
+  AccountConfigurationState,
+  eAccountSettings,
+  SettingsValue
+} from '../../../redux/account-configuration/account-configuration-slice';
 import { AccountSetting, AccountSettings } from '../configuration/settings';
 import { AccountSettingSections, AccountSettingSection } from '../configuration/sections';
 import { SettingDataBag, AccountSettingInput } from '../../../small-components/settings/account-setting-input';
@@ -15,14 +24,15 @@ import { Platforms } from '../../../data/platforms';
 import { toastAlert } from '../../../utils/toastAlert';
 import { Link } from 'react-router-dom';
 
-
 export const AccountConfiguration = () => {
   const selectedChannel = ReactUtils.GetSelectedChannel();
   const platformInfo = Platforms[selectedChannel?.channelId.toString() ?? '1'];
   const translationValues = { ...TransUtils.GetLinksValues(), ...TransUtils.GetPlatformValues(platformInfo) };
 
   const [activeTab, setActiveTab] = useState<AccountSettingSection>(AccountSettingSection.BillingAddress);
-  const sections = AccountSettingSections.filter(x => !x.ChannelIds || x.ChannelIds.includes(selectedChannel?.channelId ?? 0));
+  const sections = AccountSettingSections.filter(
+    (x) => !x.ChannelIds || x.ChannelIds.includes(selectedChannel?.channelId ?? 0)
+  );
   const bag: SettingDataBag = {
     selectedAccount: undefined
   };
@@ -53,79 +63,89 @@ export const AccountConfiguration = () => {
   const [model, setModel] = useState<Account>(initialModel);
   const [iAmBusiness, setIAMBusiness] = useState<boolean>();
 
-
   const SaveSetting = (key: eAccountSettings, value: SettingsValue) => {
-
     switch (key) {
       case eAccountSettings.Address1: {
         setModel({
-          ...model, address1: value
+          ...model,
+          address1: value
         });
         break;
       }
       case eAccountSettings.Address2: {
         setModel({
-          ...model, address2: value
+          ...model,
+          address2: value
         });
         break;
       }
       case eAccountSettings.BusinessType: {
         setModel({
-          ...model, businessType: value
+          ...model,
+          businessType: value
         });
         break;
       }
       case eAccountSettings.City: {
         setModel({
-          ...model, city: value
+          ...model,
+          city: value
         });
         break;
       }
       case eAccountSettings.Country: {
         setModel({
-          ...model, country: value
+          ...model,
+          country: value
         });
         break;
       }
       case eAccountSettings.IAmBusiness: {
         setModel({
-          ...model, iAmBusiness: value as unknown as boolean
+          ...model,
+          iAmBusiness: value as unknown as boolean
         });
         break;
       }
       case eAccountSettings.NeedInformation: {
         setModel({
-          ...model, needInformation: value as unknown as boolean
+          ...model,
+          needInformation: value as unknown as boolean
         });
         break;
       }
       case eAccountSettings.Roi: {
         setModel({
-          ...model, roi: value as unknown as boolean
+          ...model,
+          roi: value as unknown as boolean
         });
         break;
       }
       case eAccountSettings.Name: {
         setModel({
-          ...model, name: value
+          ...model,
+          name: value
         });
         break;
       }
       case eAccountSettings.Postcode: {
         setModel({
-          ...model, postcode: value
+          ...model,
+          postcode: value
         });
         break;
       }
       case eAccountSettings.Rn: {
         setModel({
-          ...model, rn: value
+          ...model,
+          rn: value
         });
         break;
       }
       case eAccountSettings.Vat: {
         setModel({
-          ...model, vat: value
+          ...model,
+          vat: value
         });
         break;
       }
@@ -143,74 +163,86 @@ export const AccountConfiguration = () => {
       switch (x.key) {
         case eAccountSettings.Address1: {
           setModel({
-            ...model, address1: x.value
+            ...model,
+            address1: x.value
           });
           break;
         }
         case eAccountSettings.Address2: {
           setModel({
-            ...model, address2: x.value
+            ...model,
+            address2: x.value
           });
           break;
         }
         case eAccountSettings.BusinessType: {
           setModel({
-            ...model, businessType: x.value
+            ...model,
+            businessType: x.value
           });
           break;
         }
         case eAccountSettings.City: {
           setModel({
-            ...model, city: x.value
+            ...model,
+            city: x.value
           });
           break;
         }
         case eAccountSettings.Country: {
           setModel({
-            ...model, country: x.value
+            ...model,
+            country: x.value
           });
           break;
         }
         case eAccountSettings.IAmBusiness: {
           setModel({
-            ...model, iAmBusiness: x.value as unknown as boolean
+            ...model,
+            iAmBusiness: x.value as unknown as boolean
           });
           setIAMBusiness(x.value === 'True' ? true : false);
           break;
         }
         case eAccountSettings.NeedInformation: {
           setModel({
-            ...model, needInformation: x.value as unknown as boolean
+            ...model,
+            needInformation: x.value as unknown as boolean
           });
           break;
         }
         case eAccountSettings.Roi: {
           setModel({
-            ...model, roi: x.value as unknown as boolean
+            ...model,
+            roi: x.value as unknown as boolean
           });
           break;
         }
         case eAccountSettings.Name: {
           setModel({
-            ...model, name: x.value
+            ...model,
+            name: x.value
           });
           break;
         }
         case eAccountSettings.Postcode: {
           setModel({
-            ...model, postcode: x.value
+            ...model,
+            postcode: x.value
           });
           break;
         }
         case eAccountSettings.Rn: {
           setModel({
-            ...model, rn: x.value
+            ...model,
+            rn: x.value
           });
           break;
         }
         case eAccountSettings.Vat: {
           setModel({
-            ...model, vat: x.value
+            ...model,
+            vat: x.value
           });
           break;
         }
@@ -232,9 +264,8 @@ export const AccountConfiguration = () => {
     console.log(rp);
     if (rp.payload?.success) {
       dispatch(getAccountConfiguration());
-      toastAlert('Account successfully deleted', 'success');
-    }
-    else {
+      toastAlert('Account successfully updated', 'success');
+    } else {
       toastAlert(rp.payload.error[0].description, 'error');
     }
     dispatch(getAccountConfiguration());
@@ -260,54 +291,68 @@ export const AccountConfiguration = () => {
       (x) => x.Section == section && (!x.ChannelIds || x.ChannelIds.includes(selectedChannel?.channelId ?? 0))
     );
     if (section === AccountSettingSection.BillingAddress) {
-      return <div>
-        <Radio.Group defaultValue={iAmBusiness} value={iAmBusiness} onChange={onChange}>
-          <Radio className='radio' checked={iAmBusiness} value={true}><h3>{t('Account.Setting.BusinessAccount')}</h3></Radio>
-          <h4>{t('Account.Setting.Description.BusinessAccount')}</h4>
-          <h4> &nbsp; </h4>
+      return (
+        <div>
+          <Radio.Group defaultValue={iAmBusiness} value={iAmBusiness} onChange={onChange}>
+            <Radio className="radio" checked={iAmBusiness} value={true}>
+              <h2>{t('Account.Setting.BusinessAccount')}</h2>
+            </Radio>
+            <p>{t('Account.Setting.Description.BusinessAccount')}</p>
+            <p> &nbsp; </p>
+            <Row>
+              <Col span={2}>&nbsp;</Col>
+              <Col span={22}>{iAmBusiness && settings.map((x) => RenderSetting(x))}</Col>
+            </Row>
+            <p> &nbsp; </p>
+            <Radio className="radio" checked={iAmBusiness} value={false}>
+              <h2>{t('Account.Setting.PersonalAccount')}</h2>
+            </Radio>
+            <p>{t('Account.Setting.Description.PersonalAccount')}</p>
+            <p></p>
+          </Radio.Group>
+          <p> &nbsp; </p>
           <Row>
-            <Col span={2}>&nbsp;</Col>
-            <Col span={22}>
-              {iAmBusiness && settings.map((x) => RenderSetting(x))}
+            <Col span={16}>&nbsp;</Col>
+            <Col span={8}>
+              <Button className="primary-btn save-btn" onClick={OnButtonClick}>
+                {t('Account.Setting.Name.SaveAll')}
+              </Button>
             </Col>
           </Row>
-          <h4> &nbsp; </h4>
-          <Radio className='radio' checked={iAmBusiness} value={false}><h3>{t('Account.Setting.PersonalAccount')}</h3></Radio>
-          <h4>{t('Account.Setting.Description.PersonalAccount')}</h4>
-          <h4></h4>
-        </Radio.Group>
-        <h4> &nbsp; </h4>
-        <Row>
-          <Col span={16}>&nbsp;</Col>
-          <Col span={8}>
-            <Button className='primary-btn save-btn' onClick={OnButtonClick}>{t('Account.Setting.Name.SaveAll')}</Button>
-          </Col>
-        </Row>
-      </div>;
-    }
-    else if (section === AccountSettingSection.PaymentMethod) {
-      return <div>
-        <Row>
-          <h1>{t('Account.PaymentMethod.SubscriptionPaymentMethod')}</h1>
-        </Row>
-        <Row>
-          <h5>{t('Account.PaymentMethod.Description.SubscriptionPaymentMethod')}<Link to='/subscriptions'>{t('Account.PaymentMethod.Description.subscriptionplans')}</Link>.</h5>
-        </Row>
-        <h3> &nbsp; </h3>
-        <Row>
-          <h1>{t('Account.PaymentMethod.AutoOrderingPM')}</h1>
-        </Row>
-        <Row>
-          <h5>{t('Account.PaymentMethod.Description.AutoOrderingWOF')}<Link to='/services/auto-ordering'>{t('Account.PaymentMethod.PremiumSuppliers')}</Link>. {t('Account.PaymentMethod.Description.AutoOrderingFSP')}</h5>
-        </Row>
-        <h6> &nbsp; </h6>
-        <Row>
-          <h5>{t('Account.PaymentMethod.Description.VAOCP')}</h5>
-        </Row>
-        <Row>
-          <Button className='primary-btn save-btn'>{t('Account.PaymentMethod.AddPaymentMentod')}</Button>
-        </Row>
-      </div>;
+        </div>
+      );
+    } else if (section === AccountSettingSection.PaymentMethod) {
+      return (
+        <div>
+          <Row>
+            <h2>{t('Account.PaymentMethod.SubscriptionPaymentMethod')}</h2>
+          </Row>
+          <Row>
+            <p>
+              {t('Account.PaymentMethod.Description.SubscriptionPaymentMethod')}
+              <Link to="/subscriptions">{t('Account.PaymentMethod.Description.subscriptionplans')}</Link>.
+            </p>
+          </Row>
+          <h2> &nbsp; </h2>
+          <Row>
+            <h2>{t('Account.PaymentMethod.AutoOrderingPM')}</h2>
+          </Row>
+          <Row>
+            <p>
+              {t('Account.PaymentMethod.Description.AutoOrderingWOF')}
+              <Link to="/services/auto-ordering">{t('Account.PaymentMethod.PremiumSuppliers')}</Link>.{' '}
+              {t('Account.PaymentMethod.Description.AutoOrderingFSP')}
+            </p>
+          </Row>
+          <p> &nbsp; </p>
+          <Row>
+            <p>{t('Account.PaymentMethod.Description.VAOCP')}</p>
+          </Row>
+          <Row>
+            <Button className="primary-btn save-btn">{t('Account.PaymentMethod.AddPaymentMentod')}</Button>
+          </Row>
+        </div>
+      );
     }
     return <>{settings.map((x) => RenderSetting(x))}</>;
   };
