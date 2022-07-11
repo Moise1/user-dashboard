2ca442ec1529de17657221c880efd62d12bde90c
Moise1 <rwibutso.mo@gmail.com>
2022-07-11 18:45:24 +0200

    initial commit

--- src/components/user/UserLogin.tsx
+++ src/components/user/UserLogin.tsx
@@ -1,12 +1,13 @@
-import { Form, Input } from 'antd';
-import { Link, withRouter } from 'react-router-dom';
+import { Checkbox, Form, Input } from 'antd';
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import { ConfirmBtn } from '../../small-components/ActionBtns';
 import { useAppDispatch, useAppSelector } from '../../custom-hooks/reduxCustomHooks';
 import { userLogin } from 'src/redux/user/userThunk';
 import { UserData } from '../../redux/user/userSlice';
+import logo from '../../assets/logoHGR.png';
 import '../../sass/user-login.scss';
 
-export const UserLogin = withRouter(({ history }) => {
+export const UserLogin = withRouter(({ history }: {history: RouteComponentProps['history']}) => {
   const { userLoading } = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
   const onFinish = (values: UserData) => {
@@ -15,17 +16,30 @@ export const UserLogin = withRouter(({ history }) => {
 
   return (
     <div className="login-form-container">
-      <h2 className="login-form-title">Login</h2>
+      <div className="login-form-header">
+        <div className="text-section">
+          <h2 className="login-form-title">Login</h2>
+          <p>
+            Don&apos;t have account yet?{' '}
+            <Link className="alternative-link" to="/signup">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+        <div>
+          <img src={logo} alt="" className="login-logo" />
+        </div>
+      </div>
       <Form
         className="login-form"
         layout="vertical"
         name="basic"
-        initialValues={{ remember: false }}
+        initialValues={{ remember: true }}
         onFinish={onFinish}
         autoComplete="on"
       >
         <Form.Item
-          label="Email"
+          label="Email address"
           name="email"
           rules={[
             { required: true, message: 'Please input your email!' },
@@ -34,24 +48,23 @@ export const UserLogin = withRouter(({ history }) => {
         >
           <Input autoFocus={true} className="auth-input" />
         </Form.Item>
+
         <Form.Item
-          label="Password"
+          label={ (
+            <div className="password-input-label">
+              <span className="label-name">Password</span>
+              <Link to="/reset-password" className='alternative-link'>Forgot your password?</Link>
+            </div>
+          )}
           name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
+          rules={[{ required: true, message: 'Please provide your password!' }]}
         >
           <Input.Password className="auth-input" />
         </Form.Item>
-
-        <Form.Item className="alternative" wrapperCol={{ offset: 6, span: 14 }}>
-          No account?{' '}
-          <span>
-            {' '}
-            <Link to="/register" className="alternative-link">
-              Create one here.
-            </Link>
-          </span>
+        <Form.Item className="alternative">
+          <Checkbox>Remember me</Checkbox>
         </Form.Item>
-        <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
+        <Form.Item>
           <ConfirmBtn htmlType="submit" disabled={userLoading}>
             {userLoading ? 'Please wait...' : 'Log In'}
           </ConfirmBtn>
