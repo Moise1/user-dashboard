5a92fe5ec95bb6af7da0c4b521e101453e62daa7
Carlos Mena <11795585+blackcid@users.noreply.github.com>
2022-07-14 13:53:48 +0200

    column renders in a different file corm columns

--- src/components/listings/Listings/columns.ts
+++ src/components/listings/Listings/columns.ts
@@ -1,10 +1,6 @@
-﻿import { Link } from 'react-router-dom';
-import { Platforms } from '../../../data/platforms';
-import { Channel } from '../../../redux/channels/channelsSlice';
-import { Source } from '../../../redux/sources/sourceSlice';
+﻿import { Source } from '../../../redux/sources/sourceSlice';
 import { ColumnData } from '../../../small-components/tables/types/columns';
-import { t } from '../../../utils/transShim';
-import { url as ApiURL } from '../../../redux/client';
+import { RenderChannelItem, RenderSource } from './columns-renders';
 
 export enum ListingColumnId {
   Image = 1,
@@ -52,54 +48,14 @@ export const ListingsColumns: ListingColumnData[] = [
     title: 'Listings.Column.ChannelItem',
     dataIndex: 'channelItem',
     width: 70,
-    render: (channelItem: string, rowR: RecordType) => {
-      const row = rowR as { channel: Channel, asin?: string, id: number };
-      const channel = row.channel;
-      if (!channel)
-        return channelItem;
-
-      const platform = Platforms[channel.channelId];
-
-      if (!platform)
-        return channelItem;
-
-      if (platform.useInternalInList) {
-        let url: string;
-        if (platform.internalUrl instanceof Object) url = platform.internalUrl[channel.isoCountry];
-        else url = platform.internalUrl;
-
-        if (!row.asin)
-          return '';
-
-        url = url
-          .replace('{asin}', row.asin ?? '')
-          .replace('{sku}', channelItem)
-          .replace('{shopName}', channel.channelIdentifier);
-        return <Link target="_blank" to={url}>{row.asin ?? ''}</Link>;
-      } else {
-        let url: string;
-        if (platform.itemUrl instanceof Object) {
-          url = platform.itemUrl[channel.isoCountry];
-        } else {
-          url = platform.itemUrl;
-        }
-        return <a target="_blank" rel="noreferrer" href={ApiURL + '/ChannelListing/BuyNow?sourceUrl=' + encodeURI(url) + '&channelListingId=' + row.id}>{channelItem}</a>;
-      }
-    }
+    render: RenderChannelItem
   },
   {
     id: ListingColumnId.Source,
     title: 'Listings.Column.Source',
     dataIndex: 'sourcePath',
     width: 70,
-    render: (path: string, rowR: RecordType) => {
-      const row = rowR as { source: Source, id: number };
-      const source = row.source;
-      if (!source)
-        return t('Listings.UnknownSource');
-      const url = 'https://' + source.baseUrl + '/' + path;
-      return <a target="_blank" rel="noreferrer" href={ApiURL + '/ChannelListing/BuyNow?sourceUrl=' + encodeURI(url) + '&channelListingId=' + row.id}>{source.name}</a>;
-    }
+    render: RenderSource
   },
   {
     id: ListingColumnId.Id,
