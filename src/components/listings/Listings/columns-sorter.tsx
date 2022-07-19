68a2bbc3f547e59e0ed3c4c0294646891fd7698e
bewaleed <105792053+bewaleed@users.noreply.github.com>
2022-07-19 20:33:15 +0500

    small fixes

--- src/components/listings/Listings/columns-sorter.tsx
+++ src/components/listings/Listings/columns-sorter.tsx
@@ -1,4 +1,4 @@
-﻿import { Source } from "../../../redux/sources/sourceSlice";
+﻿import { Source } from '../../../redux/sources/sourceSlice';
 
 type FieldValue = unknown;
 type RecordType = Record<string, FieldValue>;
@@ -11,7 +11,7 @@ type WithCost = { cost: number };
 const CompareString = (a?: string, b?: string) => (a ?? '').localeCompare(b ?? '');
 const CompareNumber = (a?: number, b?: number) => (a ?? 0) - (b ?? 0);
 
-export const SorterChanelItem = (a: RecordType, b: RecordType) => CompareString((a as WithChannelItem).channelItem, (b as WithChannelItem).channelItem)
+export const SorterChanelItem = (a: RecordType, b: RecordType) => CompareString((a as WithChannelItem).channelItem, (b as WithChannelItem).channelItem);
 export const SorterSource = (a: RecordType, b: RecordType) => CompareString((a as WithSource).source?.name, (b as WithSource).source?.name);
 export const SorterTitle = (a: RecordType, b: RecordType) => CompareString((a as WithTitle).title, (b as WithTitle).title);
 export const SorterSell = (a: RecordType, b: RecordType) => CompareNumber((a as WithSell).sell, (b as WithSell).sell);
