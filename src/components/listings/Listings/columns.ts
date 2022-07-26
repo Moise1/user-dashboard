9d886e900041b32187c86223b7f72b40845341df
Carlos Mena <11795585+blackcid@users.noreply.github.com>
2022-07-26 13:07:46 +0200

    Almost all columns

--- src/components/listings/Listings/columns.ts
+++ src/components/listings/Listings/columns.ts
@@ -1,6 +1,6 @@
 ﻿import { ColumnData } from '../../../small-components/tables/types/columns';
-import { RenderBoolean, RenderChannelItem, RenderCostOrProfit, RenderDate, RenderImage, RenderMarkup,  RenderPrice,  RenderSource, RenderStock } from './columns-renders';
-import { SorterChanelItem, SorterSource, SorterTitle, SorterSell, SorterCost, SorterProfit, SorterMarkup, SorterStock, SorterCreatedOn, SorterNotes, SorterMonitorPrice, SorterMonitorStock } from './columns-sorter';
+import { RenderBoolean, RenderChannelItem, RenderCostOrProfit, RenderDate, RenderImage, RenderMarkup,  RenderPrice,  RenderMonitorPriceDecreasePercentage,  RenderSource, RenderStock, RenderAmazonSku, RenderLowestPrice } from './columns-renders';
+import { SorterChanelItem, SorterSource, SorterTitle, SorterSell, SorterCost, SorterProfit, SorterMarkup, SorterStock, SorterCreatedOn, SorterNotes, SorterMonitorPrice, SorterMonitorStock, SorterMonitorPriceDecrease, SorterMonitorPriceDecreasePercentage, SorterIgnoreRules, SorterUnsoldDays, SorterOutOfStockDays, SorterWatches, SorterEndsOn, SorterVariation, SorterDispatchDays, SorterQuantitySold, SorterViews, SorterAsin, SorterLowestPrice, SorterBuyBox } from './columns-sorter';
 import { ListingT } from './types';
 
 export enum ListingColumnId {
@@ -18,7 +18,24 @@ export enum ListingColumnId {
   ChannelItem = 12,
   Notes = 13,
   MonitorPrice = 14,
-  MonitorStock = 15
+  MonitorStock = 15,
+  MonitorPriceDecrease = 16,
+  MonitorPriceDecreasePercentage = 17,
+  IgnoreRules = 18,
+  UnsoldDays = 19,
+  OutOfStockDays = 20,
+  OtherChannels = 21,
+  CreatedBy = 22,
+  Watchers = 23,
+  EndingOn = 24,
+  VariationOptions = 25,
+  DispatchDays = 26,
+  Sales = 27,
+  Views = 28,
+  AmazonAsin = 29,
+  AmazonSku = 30,
+  AmazonLowestPrice = 31,
+  AmazonBuyBox = 32
 }
 
 export interface ListingColumnData extends ColumnData<ListingT> {
@@ -44,6 +61,20 @@ export const ListingsColumns: ListingColumnData[] = [
     dataIndex: 'imageUrl',
     smartSearch: { ignore: true },
     render: RenderImage
+  },
+  {
+    id: ListingColumnId.AmazonAsin,
+    title: 'Listings.Column.Asin',
+    dataIndex: 'asin',
+    render: RenderChannelItem,
+    sorter: SorterAsin
+  },
+  {
+    id: ListingColumnId.AmazonSku,
+    title: 'Listings.Column.Sku',
+    dataIndex: 'channelItem',
+    render: RenderAmazonSku,
+    sorter: SorterChanelItem
   },
   {
     id: ListingColumnId.ChannelItem,
@@ -76,11 +107,32 @@ export const ListingsColumns: ListingColumnData[] = [
     sorter: SorterTitle
   },
   {
+    id: ListingColumnId.VariationOptions,
+    title: 'Listings.Column.Variation',
+    dataIndex: 'variationsText',
+    smartSearch: { ignore: true },
+    sorter: SorterVariation
+  },
+  {
     id: ListingColumnId.SellPrice,
     title: 'Listings.Column.Sell',
     dataIndex: 'channelPrice',
     render: RenderPrice,
     sorter: SorterSell
+  },
+  {
+    id: ListingColumnId.AmazonLowestPrice,
+    title: 'Listings.Column.LowestPrice',
+    dataIndex: 'lowestPrice',
+    render: RenderLowestPrice,
+    sorter: SorterLowestPrice
+  },
+  {
+    id: ListingColumnId.AmazonBuyBox,
+    title: 'Listings.Column.BuyBox',
+    dataIndex: 'buyBoxPrice',
+    render: RenderPrice,
+    sorter: SorterBuyBox
   },
   {
     id: ListingColumnId.CostPrice,
@@ -147,5 +199,79 @@ export const ListingsColumns: ListingColumnData[] = [
     smartSearch: { ignore: true },
     render: RenderBoolean,
     sorter: SorterMonitorStock
+  },
+  {
+    id: ListingColumnId.MonitorPriceDecrease,
+    title: 'Listings.Column.MonitorPriceDecrease',
+    dataIndex: 'monitorPriceDecrease',
+    smartSearch: { ignore: true },
+    render: RenderBoolean,
+    sorter: SorterMonitorPriceDecrease
+  },
+  {
+    id: ListingColumnId.MonitorPriceDecreasePercentage,
+    title: 'Listings.Column.MonitorPriceDecreasePercentage',
+    dataIndex: 'monitorPriceDecreasePercentage',
+    smartSearch: { ignore: true },
+    render: RenderMonitorPriceDecreasePercentage,
+    sorter: SorterMonitorPriceDecreasePercentage
+  },
+  {
+    id: ListingColumnId.IgnoreRules,
+    title: 'Listings.Column.IgnoreRules',
+    dataIndex: 'ignoreRules',
+    smartSearch: { ignore: true },
+    render: RenderBoolean,
+    sorter: SorterIgnoreRules
+  },
+  {
+    id: ListingColumnId.UnsoldDays,
+    title: 'Listings.Column.UnsoldDays',
+    dataIndex: 'unsoldDays',
+    smartSearch: { ignore: true },
+    sorter: SorterUnsoldDays
+  },
+  {
+    id: ListingColumnId.OutOfStockDays,
+    title: 'Listings.Column.OutOfStockDays',
+    dataIndex: 'outOfStockDays',
+    smartSearch: { ignore: true },
+    sorter: SorterOutOfStockDays
+  },
+  {
+    id: ListingColumnId.Sales,
+    title: 'Listings.Column.Sales',
+    dataIndex: 'quantitySold',
+    smartSearch: { ignore: true },
+    sorter: SorterQuantitySold
+  },
+  {
+    id: ListingColumnId.Watchers,
+    title: 'Listings.Column.Watchers',
+    dataIndex: 'watches',
+    smartSearch: { ignore: true },
+    sorter: SorterWatches
+  },
+  {
+    id: ListingColumnId.Views,
+    title: 'Listings.Column.Views',
+    dataIndex: 'views',
+    smartSearch: { ignore: true },
+    sorter: SorterViews
+  },
+  {
+    id: ListingColumnId.EndingOn,
+    title: 'Listings.Column.EndingOn',
+    dataIndex: 'endsOn',
+    smartSearch: { ignore: true },
+    render: RenderDate,
+    sorter: SorterEndsOn
+  },
+  {
+    id: ListingColumnId.DispatchDays,
+    title: 'Listings.Column.DispatchDays',
+    dataIndex: 'dispatchDays',
+    smartSearch: { ignore: true },
+    sorter: SorterDispatchDays
   }
 ];
